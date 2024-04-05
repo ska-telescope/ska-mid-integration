57f5f46d453dd53f2554c1397ae4c8be9e066e0b
SKAIN <truptin.ska@gmail.com>
2024-04-05 20:54:41 +0530

    SAH-1512: updated test file

--- tests/tmc_sdp/test_xtp-35244_long_sequence_configure_scan_scan_end.py
+++ tests/tmc_sdp/test_xtp-35244_long_sequence_configure_scan_scan_end.py
@@ -37,11 +37,77 @@ def test_tmc_sdp_long_sequences():
 
 @when(
     parsers.parse(
+        "configure and scan TMC SubarrayNode {subarray_id} "
+        "for each {scan_types} and {scan_ids}"
+    )
+)
+def execute_configure_scan_sequence(
+    subarray_node,
+    command_input_factory,
+    scan_ids,
+    event_recorder,
+    subarray_id,
+    scan_types,
+):
+    """ "A method to invoke configure and scan  command"""
+
+    check_subarray_instance(subarray_node.subarray_node, subarray_id)
+    configure_json = prepare_json_args_for_commands(
+        "configure1_mid", command_input_factory
+    )
+
+    configure_cycle = "initial"
+    processed_scan_type = ""
+
+    combined_dict = dict(zip(eval(scan_ids), eval(scan_types)))
+
+    for scan_id, scan_type in combined_dict.items():
+
+        configure_json = update_scan_type(configure_json, scan_type)
+        _, unique_id = subarray_node.store_configuration_data(configure_json)
+        if configure_cycle == "initial":
+            check_obsstate_sdp_in_first_configure(
+                event_recorder, subarray_node
+            )
+            configure_cycle = "Next"
+
+        check_configure_successful(
+            subarray_node,
+            event_recorder,
+            unique_id,
+            scan_type,
+            processed_scan_type,
+        )
+
+        scan_json = prepare_json_args_for_commands(
+            "scan_mid", command_input_factory
+        )
+
+        scan_json = update_scan_id(scan_json, scan_id)
+
+        _, unique_id = subarray_node.execute_transition(
+            "Scan", argin=scan_json
+        )
+
+        check_scan_successful(
+            subarray_node, event_recorder, scan_id, unique_id
+        )
+
+        processed_scan_type = scan_type
+
+        LOGGER.debug(
+            f"Configure-scan sequence completed for {scan_id} "
+            f"and scan_type {scan_type}"
+        )
+
+
+@when(
+    parsers.parse(
         "configure and scan TMC SubarrayNode {subarray_id} for"
         " {new_scan_types} and {new_scan_ids}"
     )
 )
-def execute_configure_scan_sequence(
+def execute_new_configure_scan_sequence(
     subarray_node,
     command_input_factory,
     scan_ids,
