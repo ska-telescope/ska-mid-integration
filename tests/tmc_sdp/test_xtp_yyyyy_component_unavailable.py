88746a6cf1d30a1c7353d23ecf1d9e660ea6a038
manthan <manthan.ska@gmail.com>
2024-04-15 18:07:59 +0530

    SAH-1513: Update path

--- tests/tmc_sdp/test_xtp_yyyyy_component_unavailable.py
+++ tests/tmc_sdp/test_xtp_yyyyy_component_unavailable.py
@@ -16,7 +16,7 @@ from tests.resources.test_harness.helpers import (
 @pytest.mark.skip(reason="SDP not reporting unavailable device to TMC.")
 @pytest.mark.tmc_sdp
 @scenario(
-    "../features/tmc_sdp/xtp-34890_sdp_component_unavailable.feature",
+    "../features/tmc_sdp/xtp_yyyyy_component_unavailable.feature",
     "SDP Subarray report the error when one of the SDP's component is"
     + " unavailable",
 )
