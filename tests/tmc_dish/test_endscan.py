00710560ad7efb4cb3e72d1cab5b588913f7db91
mansi.shirore <mansis.ska@gmail.com>
2024-04-22 16:37:36 +0530

    SAH-1524: Implemented EndScan test case

---
+++ tests/tmc_dish/test_endscan.py
@@ -0,0 +1,312 @@
+"""Test module for TMC-DISH EndScan functionality"""
+
+import time
+
+import pytest
+from pytest_bdd import given, parsers, scenario, then, when
+from ska_tango_base.control_model import ObsState
+from tango import DevState
+
+from tests.resources.test_harness.central_node_mid import CentralNodeWrapperMid
+from tests.resources.test_harness.event_recorder import EventRecorder
+from tests.resources.test_harness.helpers import (
+    prepare_json_args_for_centralnode_commands,
+    prepare_json_args_for_commands,
+)
+from tests.resources.test_harness.simulator_factory import SimulatorFactory
+from tests.resources.test_harness.subarray_node import SubarrayNodeWrapper
+from tests.resources.test_harness.utils.common_utils import JsonFactory
+from tests.resources.test_harness.utils.enums import SimulatorDeviceType
+from tests.resources.test_support.enum import DishMode, PointingState
+
+
+@pytest.mark.tmc_dish
+@scenario(
+    "../features/tmc_dish/endscan.feature",
+    "TMC executes EndScan command on DISH.LMC",
+)
+def test_tmc_dish_scan():
+    """
+    Test case to verify TMC-DISH EndScan functionality
+    """
+
+
+@given(
+    parsers.parse(
+        "a Telescope consisting of TMC, DISH {dish_ids}"
+        + ", simulated CSP and simulated SDP"
+    )
+)
+def given_a_telescope(
+    central_node_mid: CentralNodeWrapperMid,
+    simulator_factory: SimulatorFactory,
+    dish_ids: str,
+) -> None:
+    """
+    Given a TMC
+    """
+    csp_master_sim = simulator_factory.get_or_create_simulator_device(
+        SimulatorDeviceType.MID_CSP_MASTER_DEVICE
+    )
+    sdp_master_sim = simulator_factory.get_or_create_simulator_device(
+        SimulatorDeviceType.MID_SDP_MASTER_DEVICE
+    )
+
+    assert csp_master_sim.ping() > 0
+    assert sdp_master_sim.ping() > 0
+    for dish_id in dish_ids.split(","):
+        assert central_node_mid.dish_master_dict[dish_id].ping() > 0
+
+
+@given("the Telescope is in ON state")
+def turn_on_telescope(
+    central_node_mid: CentralNodeWrapperMid,
+    event_recorder: EventRecorder,
+    simulator_factory: SimulatorFactory,
+):
+    """A method to put Telescope ON"""
+    event_recorder.subscribe_event(
+        central_node_mid.dish_master_dict["SKA001"], "dishMode"
+    )
+    event_recorder.subscribe_event(
+        central_node_mid.dish_master_dict["SKA036"], "dishMode"
+    )
+    event_recorder.subscribe_event(
+        central_node_mid.dish_master_dict["SKA063"], "dishMode"
+    )
+    event_recorder.subscribe_event(
+        central_node_mid.dish_master_dict["SKA100"], "dishMode"
+    )
+
+    assert event_recorder.has_change_event_occurred(
+        central_node_mid.dish_master_dict["SKA001"],
+        "dishMode",
+        DishMode.STANDBY_LP,
+    )
+    assert event_recorder.has_change_event_occurred(
+        central_node_mid.dish_master_dict["SKA036"],
+        "dishMode",
+        DishMode.STANDBY_LP,
+    )
+    assert event_recorder.has_change_event_occurred(
+        central_node_mid.dish_master_dict["SKA063"],
+        "dishMode",
+        DishMode.STANDBY_LP,
+    )
+    assert event_recorder.has_change_event_occurred(
+        central_node_mid.dish_master_dict["SKA100"],
+        "dishMode",
+        DishMode.STANDBY_LP,
+    )
+
+    # Wait for DishMaster attribute value update,
+    # on CentralNode for value dishMode STANDBY_FP
+
+    # TODO: Improvement in tests/implementation
+    # to minimize the need of having sleep
+    time.sleep(5)
+    csp_master_sim = simulator_factory.get_or_create_simulator_device(
+        SimulatorDeviceType.MID_CSP_MASTER_DEVICE
+    )
+    sdp_master_sim = simulator_factory.get_or_create_simulator_device(
+        SimulatorDeviceType.MID_SDP_MASTER_DEVICE
+    )
+
+    event_recorder.subscribe_event(
+        central_node_mid.central_node, "telescopeState"
+    )
+
+    event_recorder.subscribe_event(csp_master_sim, "State")
+    event_recorder.subscribe_event(sdp_master_sim, "State")
+
+    assert event_recorder.has_change_event_occurred(
+        central_node_mid.central_node,
+        "telescopeState",
+        DevState.OFF,
+    )
+    central_node_mid.move_to_on()
+
+    assert event_recorder.has_change_event_occurred(
+        central_node_mid.dish_master_dict["SKA001"],
+        "dishMode",
+        DishMode.STANDBY_FP,
+    )
+    assert event_recorder.has_change_event_occurred(
+        central_node_mid.dish_master_dict["SKA036"],
+        "dishMode",
+        DishMode.STANDBY_FP,
+    )
+    assert event_recorder.has_change_event_occurred(
+        central_node_mid.dish_master_dict["SKA063"],
+        "dishMode",
+        DishMode.STANDBY_FP,
+    )
+    assert event_recorder.has_change_event_occurred(
+        central_node_mid.dish_master_dict["SKA100"],
+        "dishMode",
+        DishMode.STANDBY_FP,
+    )
+
+    # Wait for DishMaster attribute value update,
+    # on CentralNode for value dishMode STANDBY_LP
+
+    # TODO: Improvement in tests/implementation
+    # to minimize the need of having sleep
+    time.sleep(5)
+
+    assert event_recorder.has_change_event_occurred(
+        central_node_mid.sdp_master,
+        "State",
+        DevState.ON,
+    )
+
+    assert event_recorder.has_change_event_occurred(
+        central_node_mid.csp_master,
+        "State",
+        DevState.ON,
+    )
+    assert event_recorder.has_change_event_occurred(
+        central_node_mid.central_node,
+        "telescopeState",
+        DevState.ON,
+    )
+
+
+@given(parsers.parse("TMC subarray {subarray_id} is in obsState SCANNING"))
+def move_subarray_obsState_to_scanning(
+    subarray_node: SubarrayNodeWrapper,
+    command_input_factory: JsonFactory,
+    event_recorder: EventRecorder,
+    central_node_mid: CentralNodeWrapperMid,
+    subarray_id: str,
+):
+    """Method to move subarray is in Scanning obsState"""
+    event_recorder.subscribe_event(subarray_node.subarray_node, "obsState")
+
+    central_node_mid.set_subarray_id(subarray_id)
+    assign_input_json = prepare_json_args_for_centralnode_commands(
+        "assign_resources_mid", command_input_factory
+    )
+    configure_input_json = prepare_json_args_for_commands(
+        "configure_mid", command_input_factory
+    )
+    scan_input_json = prepare_json_args_for_commands(
+        "scan_mid", command_input_factory
+    )
+
+    central_node_mid.store_resources(assign_input_json)
+    assert event_recorder.has_change_event_occurred(
+        subarray_node.subarray_node,
+        "obsState",
+        ObsState.IDLE,
+    )
+
+    subarray_node.execute_transition("Configure", configure_input_json)
+    assert event_recorder.has_change_event_occurred(
+        subarray_node.subarray_node,
+        "obsState",
+        ObsState.READY,
+    )
+
+    subarray_node.execute_transition("Scan", scan_input_json)
+    assert event_recorder.has_change_event_occurred(
+        subarray_node.subarray_node,
+        "obsState",
+        ObsState.SCANNING,
+    )
+
+
+@given(
+    parsers.parse(
+        "DishMaster {dish_ids} is in dishMode OPERATE with pointingState TRACK"
+    )
+)
+def check_dish_mode_and_pointing_state_after_scan(
+    central_node_mid: CentralNodeWrapperMid,
+    event_recorder: EventRecorder,
+    dish_ids: str,
+):
+    """
+    Method to check dishMode and pointingState of DISH after scan command
+    """
+    for dish_id in dish_ids.split(","):
+        event_recorder.subscribe_event(
+            central_node_mid.dish_master_dict[dish_id], "scanID"
+        )
+        assert event_recorder.has_change_event_occurred(
+            central_node_mid.dish_master_dict[dish_id],
+            "scanID",
+            "1",
+        )
+        assert (
+            central_node_mid.dish_master_dict[dish_id].dishMode
+            == DishMode.OPERATE
+        )
+        assert (
+            central_node_mid.dish_master_dict[dish_id].pointingState
+            == PointingState.TRACK
+        )
+
+
+@when(
+    parsers.parse(
+        "I issue the EndScan command to the TMC subarray {subarray_id}"
+    )
+)
+def invoke_endscan(
+    subarray_node: SubarrayNodeWrapper,
+    subarray_id: str,
+):
+    """
+    A method to invoke EndScan command
+    """
+    subarray_node.set_subarray_id(subarray_id)
+    subarray_node.remove_scan_data()
+
+
+@then(
+    parsers.parse(
+        "the DishMaster {dish_ids} remains in "
+        + "dishMode OPERATE and pointingState TRACK"
+    )
+)
+def check_dish_mode_and_pointing_state_after_endscan(
+    central_node_mid: CentralNodeWrapperMid,
+    event_recorder: EventRecorder,
+    dish_ids: str,
+):
+    """
+    Method to check dishMode and pointingState of DISH after EndScan command.
+    """
+    for dish_id in dish_ids.split(","):
+        event_recorder.subscribe_event(
+            central_node_mid.dish_master_dict[dish_id], "scanID"
+        )
+        assert event_recorder.has_change_event_occurred(
+            central_node_mid.dish_master_dict[dish_id],
+            "scanID",
+            "1",
+        )
+        assert (
+            central_node_mid.dish_master_dict[dish_id].dishMode
+            == DishMode.OPERATE
+        )
+        assert (
+            central_node_mid.dish_master_dict[dish_id].pointingState
+            == PointingState.TRACK
+        )
+
+
+@then("TMC SubarrayNode transitions to obsState READY")
+def check_subarray_obsstate_ready(
+    subarray_node: SubarrayNodeWrapper,
+    event_recorder: EventRecorder,
+    subarray_id: str,
+):
+    """Checks if SubarrayNode's obsState attribute value is READY"""
+    subarray_node.set_subarray_id(subarray_id)
+    assert event_recorder.has_change_event_occurred(
+        subarray_node.subarray_node,
+        "obsState",
+        ObsState.READY,
+    )
