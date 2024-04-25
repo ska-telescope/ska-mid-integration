764658cbc1b824dc38db7922c071047b61d92424
manthan <manthan.ska@gmail.com>
2024-04-25 13:56:39 +0530

    SAH-1525: Unskip test cases

--- tests/tmc_csp/test_xtp_29249_on.py
+++ tests/tmc_csp/test_xtp_29249_on.py
@@ -9,7 +9,6 @@ from tests.resources.test_harness.helpers import (
 )
 
 
-@pytest.mark.skip
 @pytest.mark.tmc_csp
 @scenario(
     "../features/tmc_csp/xtp_29249_on.feature",
--- tests/tmc_csp/test_xtp_29250_29251_off_standby.py
+++ tests/tmc_csp/test_xtp_29250_29251_off_standby.py
@@ -18,7 +18,6 @@ def test_tmc_csp_telescope_off():
     """
 
 
-@pytest.mark.skip
 @pytest.mark.tmc_csp
 @scenario(
     "../features/tmc_csp/xtp_29251_standby.feature",
