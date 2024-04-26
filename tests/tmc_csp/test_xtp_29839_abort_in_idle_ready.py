9a829c38fd66b7a36e5c0720c177ca1d806f3481
manthan <manthan.ska@gmail.com>
2024-04-26 14:59:14 +0530

    SAH-1525:test pipline

--- tests/tmc_csp/test_xtp_29839_abort_in_idle_ready.py
+++ tests/tmc_csp/test_xtp_29839_abort_in_idle_ready.py
@@ -10,7 +10,7 @@ from tests.resources.test_harness.helpers import (
     prepare_json_args_for_commands,
 )
 
-
+@pytest.mark.skip
 @pytest.mark.tmc_csp
 @scenario(
     "../features/tmc_csp/xtp_29839_abort_idle_ready.feature",
