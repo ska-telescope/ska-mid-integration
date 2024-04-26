3e0aa43c518d54f873cc9e26c4d16245ea73aac7
mansi.shirore <mansis.ska@gmail.com>
2024-04-26 12:19:41 +0530

    SAH-1536: Implemented test case

---
+++ tests/tmc_dish/test_long_sequence.py
@@ -0,0 +1,198 @@
+"""Test module for long sequence functionality"""
+
+import ast
+import time
+
+import pytest
+from pytest_bdd import given, parsers, scenario, when
+from ska_tango_base.control_model import ObsState
+
+from tests.resources.test_harness.helpers import (
+    prepare_json_args_for_centralnode_commands,
+    prepare_json_args_for_commands,
+)
+from tests.resources.test_support.enum import DishMode, PointingState
+
+
+@pytest.mark.tmc_dish
+@scenario(
+    "../features/tmc_dish/long_sequence.feature",
+    "TMC executes long sequence of commands successfully",
+)
+def test_tmc_dish_long_sequence_functionality():
+    """
+    Test case to verify TMC-DISH long sequence functionality
+    """
+
+
+@given("Given a telescope in OFF or STANDBY state")
+def check_telescope_in_initial_state(
+    central_node_mid, simulator_factory, event_recorder, dish_ids
+):
+    """
+    Given a TMC
+    """
+
+    event_recorder.subscribe_event(
+        central_node_mid.dish_master_dict["SKA001"], "dishMode"
+    )
+    event_recorder.subscribe_event(
+        central_node_mid.dish_master_dict["SKA036"], "dishMode"
+    )
+    event_recorder.subscribe_event(
+        central_node_mid.dish_master_dict["SKA063"], "dishMode"
+    )
+    event_recorder.subscribe_event(
+        central_node_mid.dish_master_dict["SKA100"], "dishMode"
+    )
+    event_recorder.subscribe_event(
+        central_node_mid.central_node, "telescopeState"
+    )
+
+    assert event_recorder.has_change_event_occurred(
+        central_node_mid.dish_master_dict["SKA001"],
+        "dishMode",
+        DishMode.STANDBY_LP,
+    )
+    assert event_recorder.has_change_event_occurred(
+        central_node_mid.dish_master_dict["SKA036"],
+        "dishMode",
+        DishMode.STANDBY_LP,
+    )
+    assert event_recorder.has_change_event_occurred(
+        central_node_mid.dish_master_dict["SKA063"],
+        "dishMode",
+        DishMode.STANDBY_LP,
+    )
+    assert event_recorder.has_change_event_occurred(
+        central_node_mid.dish_master_dict["SKA100"],
+        "dishMode",
+        DishMode.STANDBY_LP,
+    )
+    # Wait for DishMaster attribute value update,
+    # on CentralNode for value dishMode STANDBY_LP
+
+    # TODO: Improvement in tests/implementation
+    # to minimize the need of having sleep
+
+    time.sleep(5)
+    assert central_node_mid.telescope_state in ["OFF", "STANDBY"]
+
+
+@when(parsers.parse("I assign {resources} to TMC subarray {subarray_id}"))
+def move_subarray_to_obsState_idle(
+    subarray_node,
+    central_node_mid,
+    event_recorder,
+    command_input_factory,
+    resources,
+    subarray_id,
+):
+    """
+    Method to check subarray is in IDLE obsState
+    """
+    event_recorder.subscribe_event(subarray_node.subarray_node, "obsState")
+    central_node_mid.set_subarray_id(subarray_id)
+    assign_input_json = prepare_json_args_for_centralnode_commands(
+        "assign_resources_mid", command_input_factory
+    )
+
+    central_node_mid.store_resources(assign_input_json)
+
+    assert event_recorder.has_change_event_occurred(
+        subarray_node.subarray_node,
+        "obsState",
+        ObsState.IDLE,
+    )
+    assert event_recorder.has_change_event_occurred(
+        central_node_mid.subarray_node,
+        "assignedResources",
+        ast.literal_eval(resources),  # casts string coded tuple to tuple
+    )
+
+
+@when(
+    parsers.parse(
+        "I configure the subarray {subarray_id} with receiver_band_1"
+    )
+)
+def configure_subarray(
+    subarray_node,
+    central_node_mid,
+    event_recorder,
+    command_input_factory,
+    resources,
+    subarray_id,
+):
+    """
+    A method to invoke Configure command
+    """
+    configure_input_json = prepare_json_args_for_commands(
+        "configure_mid", command_input_factory
+    )
+    central_node_mid.set_subarray_id(subarray_id)
+    subarray_node.store_configuration_data(configure_input_json)
+    for dish_id in ["SKA001", "SKA036", "SKA063", "SKA100"]:
+        event_recorder.subscribe_event(
+            central_node_mid.dish_master_dict[dish_id], "pointingState"
+        )
+        event_recorder.subscribe_event(
+            central_node_mid.dish_master_dict[dish_id], "dishMode"
+        )
+
+        assert event_recorder.has_change_event_occurred(
+            central_node_mid.dish_master_dict[dish_id],
+            "dishMode",
+            DishMode.OPERATE,
+        )
+
+        assert event_recorder.has_change_event_occurred(
+            central_node_mid.dish_master_dict[dish_id],
+            "pointingState",
+            PointingState.TRACK,
+        )
+    assert event_recorder.has_change_event_occurred(
+        subarray_node.subarray_node,
+        "obsState",
+        ObsState.READY,
+    )
+
+
+@when(parsers.parse("I issue End command to the subarray {subarray_id}"))
+def end_configuration_on_subarray(
+    subarray_node,
+    central_node_mid,
+    event_recorder,
+    command_input_factory,
+    resources,
+    subarray_id,
+):
+    """
+    A method to invoke end command
+    """
+    central_node_mid.set_subarray_id(subarray_id)
+    subarray_node.end_observation()
+    for dish_id in ["SKA001", "SKA036", "SKA063", "SKA100"]:
+        event_recorder.subscribe_event(
+            central_node_mid.dish_master_dict[dish_id], "pointingState"
+        )
+        event_recorder.subscribe_event(
+            central_node_mid.dish_master_dict[dish_id], "dishMode"
+        )
+
+        assert event_recorder.has_change_event_occurred(
+            central_node_mid.dish_master_dict[dish_id],
+            "dishMode",
+            DishMode.OPERATE,
+        )
+
+        assert event_recorder.has_change_event_occurred(
+            central_node_mid.dish_master_dict[dish_id],
+            "pointingState",
+            PointingState.TRACK,
+        )
+    assert event_recorder.has_change_event_occurred(
+        subarray_node.subarray_node,
+        "obsState",
+        ObsState.IDLE,
+    )
