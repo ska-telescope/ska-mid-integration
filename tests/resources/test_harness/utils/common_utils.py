6320ff2f022086069e6cca2ac61df83c0d2c4f35
manthan <manthan.ska@gmail.com>
2024-04-26 16:52:42 +0530

    SAH-1525: Test pipline

--- tests/resources/test_harness/utils/common_utils.py
+++ tests/resources/test_harness/utils/common_utils.py
@@ -126,7 +126,7 @@ def check_scan_successful(
     the_waiter.set_wait_for_specific_obsstate(
         "READY", [subarray_node.subarray_node]
     )
-    the_waiter.wait(100)
+    the_waiter.wait(150)
     assert event_recorder.has_change_event_occurred(
         subarray_node.subarray_devices["csp_subarray"],
         "obsState",
