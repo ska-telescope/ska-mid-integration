30e263598583a050ba2731e09e41235713c5c215
apurvapatkar <apurva.ska@gmail.com>
2024-04-30 17:24:49 +0530

    SAH-1511: Code cleanup.

--- tests/test_cases_old/test_configure_end_command.py
+++ tests/test_cases_old/test_configure_end_command.py
@@ -213,6 +213,10 @@ def test_configure_timeout_sdp(json_factory, change_event_callbacks):
         )
 
 
+@pytest.mark.skip(
+    reason="Fails intermittently due to dish unavailability"
+    + " error. Will be resolved under story SAH-1531."
+)
 @pytest.mark.SKA_mid
 def test_configure_error_propagation_sdp(json_factory, change_event_callbacks):
     """Verify timeout exception raised when csp set to defective."""
