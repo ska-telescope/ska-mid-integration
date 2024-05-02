fe3b0ac734eacf5f0a20f81ad75005d366eb319d
manthan <manthan.ska@gmail.com>
2024-05-02 15:41:16 +0530

    SAH-1525: Test pipline

--- tests/tmc_csp/test_xtp_29259_assign.py
+++ tests/tmc_csp/test_xtp_29259_assign.py
@@ -12,7 +12,6 @@ from tests.resources.test_harness.helpers import (
 )
 
 
-@pytest.mark.skip
 @pytest.mark.tmc_csp
 @scenario(
     "../features/tmc_csp/xtp_29259_assign.feature",
--- tests/tmc_csp/test_xtp_29260_release.py
+++ tests/tmc_csp/test_xtp_29260_release.py
@@ -11,7 +11,6 @@ from tests.resources.test_harness.helpers import (
 )
 
 
-@pytest.mark.skip
 @pytest.mark.tmc_csp
 @scenario(
     "../features/tmc_csp/xtp_29260_release.feature",
