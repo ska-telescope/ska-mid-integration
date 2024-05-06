36f1344de269767c743f626240a7e4816ade35da
manthan <manthan.ska@gmail.com>
2024-05-06 19:44:06 +0530

    SAH-1525: Unskip test case

--- tests/tmc_csp/test_xtp_29374_end.py
+++ tests/tmc_csp/test_xtp_29374_end.py
@@ -19,7 +19,6 @@ from tests.resources.test_harness.utils.common_utils import JsonFactory
 LOGGER = logging.getLogger(__name__)
 
 
-@pytest.mark.skip(reason="Requires CBF chart with ska-telmodel v.1.15.0")
 @pytest.mark.tmc_csp
 @scenario(
     "../features/tmc_csp/xtp_29374_end.feature",
