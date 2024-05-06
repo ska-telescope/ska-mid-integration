45d954dfc210b563f2895e1716d38c04be55f1a4
JayantKumbhar <jayant.ska@gmail.com>
2024-05-06 17:32:55 +0530

    SAH-1526: Resvert a change done as part of comment resolution

--- tests/tmc_dish/test_xtp-42757_successive_scan.py
+++ tests/tmc_dish/test_xtp-42757_successive_scan.py
@@ -81,9 +81,7 @@ def turn_on_telescope(
         DevState.ON,
     )
 
-    event_recorder.subscribe_event(
-        subarray_node.subarray_node, "obsState", 500.0
-    )
+    event_recorder.subscribe_event(subarray_node.subarray_node, "obsState")
 
     assign_input_json = prepare_json_args_for_centralnode_commands(
         "assign_resources_mid", command_input_factory
@@ -188,9 +186,9 @@ def check_automatic_endscan_with_scan_duration1(
     """
     A method to check if EndScan is successful.
     """
-    # time.sleep(int(scan_duration1))
+    time.sleep(int(scan_duration1))
     assert event_recorder.has_change_event_occurred(
-        subarray_node.subarray_node, "obsState", ObsState.READY, lookahead=15
+        subarray_node.subarray_node, "obsState", ObsState.READY, lookahead=10
     )
 
 
@@ -206,9 +204,9 @@ def check_automatic_endscan_with_scan_duration2(
     """
     A method to check if EndScan is successful.
     """
-    # time.sleep(int(scan_duration2))
+    time.sleep(int(scan_duration2))
     assert event_recorder.has_change_event_occurred(
-        subarray_node.subarray_node, "obsState", ObsState.READY, lookahead=15
+        subarray_node.subarray_node, "obsState", ObsState.READY, lookahead=10
     )
 
 
