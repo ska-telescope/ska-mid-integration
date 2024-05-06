6285f8361ce54e3ee292acee84ffc92bfb266d2b
mansi.shirore <mansis.ska@gmail.com>
2024-05-06 18:13:28 +0530

    SAH-1524: Update test case

--- tests/tmc_dish/test_xtp-29417_end.py
+++ tests/tmc_dish/test_xtp-29417_end.py
@@ -279,7 +279,7 @@ def check_dish_mode_and_pointing_state(
             "dishMode",
             DishMode.OPERATE,
         )
-        time.sleep(20)
+        time.sleep(40)
         LOGGER.info(
             "PointingState %s",
             central_node_mid.dish_master_dict[dish_id].pointingState,
