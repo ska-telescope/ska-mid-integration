ba79ca92e4d237e6d603dc9d40f4565dbb2f5e23
manthan <manthan.ska@gmail.com>
2024-05-06 20:47:37 +0530

    SAH-1522: skip test case

--- tests/tmc_harness_testcases/bdd_scenarios/test_xtp_35778_delay_model.py
+++ tests/tmc_harness_testcases/bdd_scenarios/test_xtp_35778_delay_model.py
@@ -22,7 +22,7 @@ from tests.resources.test_harness.utils.common_utils import JsonFactory
 
 LOGGER = logging.getLogger(__name__)
 
-
+@pytest.mark.skip
 @pytest.mark.SKA_mid
 @scenario(
     "../features/test_harness/xtp_35778_test_delay_model.feature",
