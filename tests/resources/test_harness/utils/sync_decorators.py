0a12814e8a98dae383fc36ac73764f9a84d8b532
JayantKumbhar <jayant.ska@gmail.com>
2024-05-06 10:14:44 +0530

    SAH-1526: Revert configure timeout to 500

--- tests/resources/test_harness/utils/sync_decorators.py
+++ tests/resources/test_harness/utils/sync_decorators.py
@@ -144,9 +144,9 @@ def sync_configure(device_dict):
             result = func(*args, **kwargs)
             if invoked_from_ready:
                 the_waiter.set_wait_for_configuring()
-                the_waiter.wait(3600)
+                the_waiter.wait(500)
             the_waiter.set_wait_for_configure()
-            the_waiter.wait(3600)
+            the_waiter.wait(500)
             return result
 
         return wrapper
