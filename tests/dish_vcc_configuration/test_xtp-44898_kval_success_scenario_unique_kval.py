2e9c26755f0710f3b7fec2b05181cb97c7db5bd7
Akshay Sonawane <akshayss.ska@gmail.com>
2024-05-08 22:01:45 +0530

    HM-453: update the scenarios with xtp

--- tests/dish_vcc_configuration/test_xtp-44898_kval_success_scenario_unique_kval.py
+++ tests/dish_vcc_configuration/test_xtp-44898_kval_success_scenario_unique_kval.py
@@ -1,4 +1,5 @@
 import json
+import time
 
 import pytest
 from pytest_bdd import given, scenario, then, when
@@ -119,6 +120,7 @@ def move_subarray_node_to_idle_obsstate(
         (unique_id[0], str(int(ResultCode.OK))),
         lookahead=5,
     )
+    time.sleep(3)
 
 
 @then("I successfully invoke Configure command on TMC")
