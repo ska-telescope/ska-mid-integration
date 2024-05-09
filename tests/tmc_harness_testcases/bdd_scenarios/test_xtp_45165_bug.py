af7e017c84f705d0cd3a96d73192233943e9e2e7
manthan <manthan.ska@gmail.com>
2024-05-09 21:37:54 +0530

    SAH-1522: Update test case

--- tests/tmc_harness_testcases/bdd_scenarios/test_xtp_45165_bug.py
+++ tests/tmc_harness_testcases/bdd_scenarios/test_xtp_45165_bug.py
@@ -62,8 +62,7 @@ def move_subarray_node_to_idle_obsstate(
     # Create json for AssignResources commands with requested subarray_id
     assign_input = json.loads(assign_input_json)
     assign_input["subarray_id"] = int(subarray_id)
-    central_node_mid.store_resources(json.dumps(assign_input))
-    _, unique_id = central_node_mid.store_resources(assign_input_json)
+    _, unique_id = central_node_mid.store_resources(json.dumps(assign_input))
     event_recorder.subscribe_event(subarray_node.subarray_node, "obsState")
     assert event_recorder.has_change_event_occurred(
         central_node_mid.subarray_node,
