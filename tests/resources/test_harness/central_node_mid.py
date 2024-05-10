3f18bd564a284651e3f309de44b7c3d6e0e145e8
mansi.shirore <mansis.ska@gmail.com>
2024-05-10 12:40:16 +0530

    SAH-1524: Disable setDirectDishmode from centralnodemid module

--- tests/resources/test_harness/central_node_mid.py
+++ tests/resources/test_harness/central_node_mid.py
@@ -567,9 +567,9 @@ class CentralNodeWrapperMid(CentralNodeWrapper):
             device_proxy.SetDirectState(subarray_state)
 
         # If Dish master provided then set it to standby
-        if self.dish_master_list:
-            for device in self.dish_master_list:
-                device.SetDirectDishMode(dish_mode)
+        # if self.dish_master_list:
+        #     for device in self.dish_master_list:
+        #         device.SetDirectDishMode(dish_mode)
 
     def set_values_with_sdp_dish_mocks(
         self, subarray_state: DevState, dish_mode: DishMode
