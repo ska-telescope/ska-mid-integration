e0cb50febe1ac317fdad3140a3d3fa3ced1585a6
mansi.shirore <mansis.ska@gmail.com>
2024-05-13 22:36:40 +0530

    SAH-1524: Update test case

--- tests/tmc_sdp/test_xtp-29382_configure.py
+++ tests/tmc_sdp/test_xtp-29382_configure.py
@@ -101,7 +101,7 @@ def invoke_configure(
     input_json["sdp"]["scan_type"] = scan_type
     central_node_mid.set_subarray_id(subarray_id)
     pytest.command_result = subarray_node.execute_transition(
-        "Configure", argin=input_json
+        "Configure", argin=json.dumps(input_json)
     )
 
 
