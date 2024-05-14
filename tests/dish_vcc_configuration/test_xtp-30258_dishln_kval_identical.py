6ec4f31f9cefdd20bde985ef95be4acd5abb2a5c
Akshay Sonawane <akshayss.ska@gmail.com>
2024-05-14 11:14:29 +0530

    HM-453: update the test case

--- tests/dish_vcc_configuration/test_xtp-30258_dishln_kval_identical.py
+++ tests/dish_vcc_configuration/test_xtp-30258_dishln_kval_identical.py
@@ -12,7 +12,6 @@ from tests.resources.test_harness.helpers import (
 from tests.resources.test_support.common_utils.result_code import ResultCode
 
 
-@pytest.mark.skip
 @pytest.mark.SKA_mid
 @scenario(
     "../features/dish_vcc_initialization/"
