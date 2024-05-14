27f67c1fa9d637e73aab630f849b006077eb970d
manthan <manthan.ska@gmail.com>
2024-05-14 16:06:59 +0530

    SAH-1529: Unskip test case

--- tests/tmc_dish/test_xtp-29077.py
+++ tests/tmc_dish/test_xtp-29077.py
@@ -16,7 +16,6 @@ from tests.resources.test_harness.utils.enums import SimulatorDeviceType
 from tests.resources.test_support.enum import DishMode
 
 
-@pytest.mark.skip
 @pytest.mark.tmc_dish
 @scenario(
     "../features/tmc_dish/xtp-29077.feature",
