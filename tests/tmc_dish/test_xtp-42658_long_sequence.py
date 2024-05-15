64954f201a874b4b06d0b7e2e5b221fa99f5bd70
mansi.shirore <mansis.ska@gmail.com>
2024-05-15 20:25:03 +0530

    SAH-1536: Update the test case.

--- tests/tmc_dish/test_xtp-42658_long_sequence.py
+++ tests/tmc_dish/test_xtp-42658_long_sequence.py
@@ -701,6 +701,7 @@ def end_configuration_on_subarray(
     central_node_mid.set_subarray_id(subarray_id)
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     pytest.command_result = subarray_node.end_observation()
 =======
     pytest.command_result = subarray_node.execute_transition("End")
@@ -708,6 +709,9 @@ def end_configuration_on_subarray(
 =======
     pytest.command_result = subarray_node.end_observation()
 >>>>>>> 091f5628 (SAH-1536: Update test case.)
+=======
+    pytest.command_result = subarray_node.execute_transition("End")
+>>>>>>> a3a8fb20 (SAH-1536: Update the test case.)
     for dish_id in ["SKA001", "SKA036", "SKA063", "SKA100"]:
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -893,8 +897,8 @@ def reconfigure_subarray(
     configure_input_json["dish"]["receiver_band"] = receiver_band_2
     configure_input_json["csp"]["common"]["frequency_band"] = "2"
     central_node_mid.set_subarray_id(subarray_id)
-    pytest.command_result = subarray_node.store_configuration_data(
-        json.dumps(configure_input_json)
+    pytest.command_result = subarray_node.execute_transition(
+        "Configure", json.dumps(configure_input_json)
     )
     for dish_id in ["SKA001", "SKA036", "SKA063", "SKA100"]:
 <<<<<<< HEAD
