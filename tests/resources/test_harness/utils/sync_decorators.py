c894953b6d354e009de51dcd482f5818c7e99e1b
manthan <manthan.ska@gmail.com>
2024-05-16 14:16:24 +0530

    SAH-1529: Increase timeout

--- tests/resources/test_harness/utils/sync_decorators.py
+++ tests/resources/test_harness/utils/sync_decorators.py
@@ -5,7 +5,7 @@ from tests.resources.test_harness.utils.wait_helpers import Waiter
 from tests.resources.test_support.common_utils.base_utils import DeviceUtils
 from tests.resources.test_support.common_utils.common_helpers import Resource
 
-TIMEOUT = 500
+TIMEOUT = 1000
 
 
 def sync_telescope_on(func):
