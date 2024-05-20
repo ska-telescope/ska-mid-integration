9de1c35270cbfd7a7ceb3597b6b64c602072e04f
manthan <manthan.ska@gmail.com>
2024-05-20 22:46:40 +0530

    SAH-1525: Update make file

--- tests/resources/test_harness/central_node_mid.py
+++ tests/resources/test_harness/central_node_mid.py
@@ -69,6 +69,7 @@ class CentralNodeWrapperMid(CentralNodeWrapper):
         self.central_node = DeviceProxy(centralnode)
         self.central_node.set_timeout_millis(5000)
         self.subarray_node = DeviceProxy(tmc_subarraynode1)
+        self.subarray_node.set_timeout_millis(5000)
         self.csp_master_leaf_node = DeviceProxy(tmc_csp_master_leaf_node)
         self.sdp_master_leaf_node = DeviceProxy(tmc_sdp_master_leaf_node)
         self.sdp_master = DeviceProxy(sdp_master)
