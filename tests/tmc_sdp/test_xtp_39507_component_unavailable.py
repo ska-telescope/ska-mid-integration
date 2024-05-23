50c23911adf3c3762ec1b23046de9670f56fdc45
mansi.shirore <mansis.ska@gmail.com>
2024-05-23 20:39:38 +0530

    SAH-1513: Update test case.

--- tests/tmc_sdp/test_xtp_39507_component_unavailable.py
+++ tests/tmc_sdp/test_xtp_39507_component_unavailable.py
@@ -6,12 +6,17 @@ import pytest
 from pytest_bdd import given, parsers, scenario, then, when
 from ska_control_model import ObsState
 from ska_tango_base.control_model import HealthState
-from ska_tango_testing.mock.placeholders import Anything
 
+from tests.resources.test_harness.central_node_mid import CentralNodeWrapperMid
+from tests.resources.test_harness.event_recorder import EventRecorder
 from tests.resources.test_harness.helpers import (
+    check_for_device_command_event,
     generate_eb_pb_ids,
+    get_master_device_simulators,
     prepare_json_args_for_centralnode_commands,
 )
+from tests.resources.test_harness.simulator_factory import SimulatorFactory
+from tests.resources.test_harness.subarray_node import SubarrayNodeWrapper
 
 
 @pytest.mark.tmc_sdp_unhappy
@@ -28,12 +33,35 @@ def test_sdp_component_unavailable(central_node_mid):
 
 
 @given("a Telescope consisting of TMC,SDP,simulated CSP and simulated Dish")
-def given_tmc_with_simulated_csp_dish():
+def given_tmc_with_simulated_csp_dish(
+    central_node_mid: CentralNodeWrapperMid,
+    subarray_node: SubarrayNodeWrapper,
+    simulator_factory: SimulatorFactory,
+):
     """A method to check if tmc subsystems are simulated."""
 
     assert os.environ.get("DISH_SIMULATION_ENABLED") == "true"
     assert os.environ.get("SDP_SIMULATION_ENABLED") == "false"
     assert os.environ.get("CSP_SIMULATION_ENABLED") == "true"
+    (
+        csp_master_sim,
+        _,
+        dish_master_sim_1,
+        dish_master_sim_2,
+        dish_master_sim_3,
+        dish_master_sim_4,
+    ) = get_master_device_simulators(simulator_factory)
+    assert central_node_mid.central_node.ping() > 0
+    assert central_node_mid.sdp_master.ping() > 0
+    assert central_node_mid.subarray_devices["sdp_subarray"].ping() > 0
+    assert central_node_mid.subarray_devices["csp_subarray"].ping() > 0
+    assert subarray_node.sdp_subarray_leaf_node.ping() > 0
+    assert subarray_node.csp_subarray_leaf_node.ping() > 0
+    assert csp_master_sim.ping() > 0
+    assert dish_master_sim_1.ping() > 0
+    assert dish_master_sim_2.ping() > 0
+    assert dish_master_sim_3.ping() > 0
+    assert dish_master_sim_4.ping() > 0
 
 
 @given("the subarray is in EMPTY obsState")
@@ -51,7 +79,7 @@ def subarray_is_in_empty_obsstate(
 
 
 @when("one of the SDP's component subsystem is made unavailable")
-def sdp_proc_controller_unavailable(central_node_mid):
+def sdp_proc_controller_unavailable(central_node_mid: CentralNodeWrapperMid):
     """
     Processing controller is made unavailable in gitlab script, asserted SDP
     Controller's HealthState as degraded to verify the same
@@ -61,7 +89,9 @@ def sdp_proc_controller_unavailable(central_node_mid):
 
 @when(parsers.parse("I assign resources to the subarray {subarray_id}"))
 def tmc_assign_resources_invoke(
-    central_node_mid, subarray_id, command_input_factory
+    central_node_mid: CentralNodeWrapperMid,
+    subarray_id: str,
+    command_input_factory,
 ):
     """
     Method to invoke AssignResources command.
@@ -78,24 +108,27 @@ def tmc_assign_resources_invoke(
 
 
 @then("SDP subarray report the unavailability of SDP Component")
-def sdp_subarray_reports_unavailability(event_recorder, central_node_mid):
+def sdp_subarray_reports_unavailability(
+    event_recorder: EventRecorder, subarray_node: SubarrayNodeWrapper
+):
     """
     Method to verify SDP subarray reports unavailability to TMC.
     """
     event_recorder.subscribe_event(
-        central_node_mid.central_node,
+        subarray_node.sdp_subarray_leaf_node,
         "longRunningCommandResult",
     )
     exception_message = (
         " The processing controller, helm deployer, or both are OFFLINE:"
         + " cannot start processing blocks.\n"
     )
-    pytest.assertion_data = event_recorder.has_change_event_occurred(
-        central_node_mid.central_node,
-        attribute_name="longRunningCommandResult",
-        attribute_value=(pytest.unique_id[0], Anything),
+    assert check_for_device_command_event(
+        subarray_node.sdp_subarray_leaf_node,
+        "longRunningCommandResult",
+        exception_message,
+        event_recorder,
+        "AssignResources",
     )
-    assert exception_message in pytest.assertion_data["attribute_value"][1]
 
 
 @then("TMC should report the error to client")
@@ -115,7 +148,9 @@ def tmc_reports_unavailability_to_client():
 
 
 @then(parsers.parse("the TMC SubarrayNode {subarray_id} stuck in RESOURCING"))
-def tmc_stuck_in_resourcing(subarray_node, event_recorder):
+def tmc_stuck_in_resourcing(
+    subarray_node: SubarrayNodeWrapper, event_recorder: EventRecorder
+):
     """
     Method to verify the subarray stuck in RESOURCING obsstate
     """
