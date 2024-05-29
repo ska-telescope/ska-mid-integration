ab3fb451ad6c236c996ff7222c744959f5079376
manthan <manthan.ska@gmail.com>
2024-05-29 11:43:04 +0530

    SAH-1525: skip test case

--- tests/tmc_csp/test_xtp-42551_long_sequence_with_end.py
+++ tests/tmc_csp/test_xtp-42551_long_sequence_with_end.py
@@ -21,7 +21,7 @@ from tests.resources.test_harness.utils.common_utils import (
 configure_logging(logging.DEBUG)
 LOGGER = logging.getLogger(__name__)
 
-
+@pytest.mark.skip
 @pytest.mark.tmc_csp
 @scenario(
     "../features/tmc_csp/xtp_40175_long_sequence_configure_scan.feature",
