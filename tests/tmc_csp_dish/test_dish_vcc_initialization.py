1915c2441eeba163e0c1a3bee4297641a8185a8d
apurvapatkar <apurva.ska@gmail.com>
2024-05-30 12:41:50 +0530

    SAH-1529: Execute tests.

--- tests/tmc_csp_dish/test_dish_vcc_initialization.py
+++ tests/tmc_csp_dish/test_dish_vcc_initialization.py
@@ -43,9 +43,11 @@ def given_tmc_using_default_version(tmc_mid, command_input_factory):
 
     logging.info("In GIVEN 01 -------------------------")
     for dish in tmc_mid.central_node.dish_master_list:
+        logging.info("Dish device name", dish.dev_name())
         logging.info("Dish dishMode: %s", dish.dishMode)
 
     for dish_leaf in tmc_mid.central_node.dish_leaf_node_list:
+        logging.info("Dish Leaf device name", dish_leaf.dev_name())
         logging.info("Dish Leaf dishMode: %s", dish_leaf.dishMode)
 
 
