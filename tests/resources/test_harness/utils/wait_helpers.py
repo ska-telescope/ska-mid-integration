1de57bc5b2627f9d16364ed3ba4b2bfeb2563c2a
apurvapatkar <apurva.ska@gmail.com>
2024-05-30 11:41:19 +0530

    SAH-1529: Execute tests.

--- tests/resources/test_harness/utils/wait_helpers.py
+++ tests/resources/test_harness/utils/wait_helpers.py
@@ -45,17 +45,24 @@ class Waiter:
     def set_wait_for_dish(self, attribute_name, state_name):
         """Set wait for dish"""
         for dish_master in self.dish_master_list:
-            self.waits.append(
-                watch(Resource(dish_master)).to_become(
-                    attribute_name, changed_to=state_name
+            LOGGER.info("dish master dishMode: %s", dish_master.dishMode)
+            LOGGER.info("State name: %s", state_name)
+            if dish_master.dishMode != state_name:
+                self.waits.append(
+                    watch(Resource(dish_master)).to_become(
+                        attribute_name, changed_to=state_name
+                    )
                 )
-            )
+
         for dish_leaf_node in self.dish_leaf_node_list:
-            self.waits.append(
-                watch(Resource(dish_leaf_node)).to_become(
-                    attribute_name, changed_to=state_name
+            LOGGER.info("dish leaf node dishMode: %s", dish_leaf_node.dishMode)
+            LOGGER.info("State name: %s", state_name)
+            if dish_leaf_node.dishMode != state_name:
+                self.waits.append(
+                    watch(Resource(dish_leaf_node)).to_become(
+                        attribute_name, changed_to=state_name
+                    )
                 )
-            )
 
     def set_wait_for_going_to_off(self):
         self.waits.append(
