9133f80ced4a3545acaf07cd78efcf1146c93905
yogeshk.ska <yogeshk.ska@gmail.com>
2024-06-17 17:34:33 +0530

    HM-462: trackloadstaticoff assertion removed

--- tests/tmc_harness_testcases/bdd_scenarios/test_science_scan_after_five_point_calibration.py
+++ tests/tmc_harness_testcases/bdd_scenarios/test_science_scan_after_five_point_calibration.py
@@ -208,11 +208,6 @@ def subarray_applies_calibration_solutions_to_dishes(
     """Then the Subarray fetches and applies the configuration solutions to the
     dishes."""
 
-    for dish_master in subarray_node.dish_master_list:
-        check_long_running_command_status_events(
-            event_recorder, dish_master, "TrackLoadStaticOff"
-        )
-
     assert wait_and_validate_device_attribute_value(
         subarray_node.dish_leaf_node_list[0],
         "lastPointingData",
