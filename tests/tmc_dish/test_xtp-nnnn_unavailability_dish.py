2f4b8983e4e4fe70afb135bac203ebe10e195f24
JayantKumbhar <jayant.ska@gmail.com>
2024-07-08 12:42:05 +0530

    SAH-1536: Enable tmc-dish skipped tests

--- tests/tmc_dish/test_xtp-nnnn_unavailability_dish.py
+++ tests/tmc_dish/test_xtp-nnnn_unavailability_dish.py
@@ -28,7 +28,6 @@ from tests.resources.test_support.enum import DishMode
 # from tango import DevState
 
 
-@pytest.mark.skip
 @pytest.mark.tmc_dish
 @scenario(
     "../features/tmc_dish/xtp-nnnn_unavailability_dish.feature",
