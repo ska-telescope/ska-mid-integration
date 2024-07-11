6533b9adb43b43fc5a1f07000e348e7ff0241329
Emanuele Lena <emanuele.lena@designcoaching.net>
2024-07-11 11:09:44 +0200

    OMCPT-40: THR - refactored harness v3 - add note about anticipating the test harness tear down

--- tests/tmc_csp/conftest.py
+++ tests/tmc_csp/conftest.py
@@ -31,6 +31,7 @@ from tests.test_harness2.sut_initialization.harness_components_factory import (
 from tests.test_harness2.sut_structure.sut_wrapper import (
     TelescopeWrapper as TelescopeWrapper2,
 )
+from tests.test_harness3.telescope_actions.subarray.set_subarray_id import SetSubarrayId
 from tests.test_harness3.telescope_facades.csp_facade import CSPFacade
 from tests.test_harness3.telescope_facades.dishes_facade import DishesFacade
 from tests.test_harness3.telescope_facades.sdp_facade import SDPFacade
@@ -321,6 +322,10 @@ def telescope_wrapper() -> TelescopeWrapper:
     yield telescope
     telescope.tear_down()
 
+    # NOTE: As the code is organized now, I cannot anticipate the
+    # teardown of the telescope structure. To run reset now I should
+    # init subarray node (with SetSubarrayId), but to do that I need
+    # to know subarray_id, which is a parameter of the Gherkin steps.
 
 @fixture
 def central_node_facade(telescope_wrapper: TelescopeWrapper):
