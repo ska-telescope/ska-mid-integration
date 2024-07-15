c8e3a788b3b7422e8351159ec1edb8805b55aa88
apurvapatkar <apurva.ska@gmail.com>
2024-07-15 14:32:06 +0530

    SAH-1558: Fix the error in the test.

--- tests/resources/test_harness/event_recorder.py
+++ tests/resources/test_harness/event_recorder.py
@@ -110,7 +110,8 @@ class EventRecorder(object):
             except AssertionError:
                 device_name = device.dev_name()
                 LOGGER.info("Device name is: %s", device_name)
-                full_attr_name = device_name + attribute_name
+                full_attr_name = device_name + "/" + attribute_name
+                LOGGER.info("full_attr_name is: %s", full_attr_name)
                 attr_proxy = AttributeProxy(full_attr_name)
                 LOGGER.info("Attribute value is: %s", attr_proxy.read())
                 return False
