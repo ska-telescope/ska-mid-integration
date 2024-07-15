67a86fa272ef6cc108a35b9a313533e706d80b9e
apurvapatkar <apurva.ska@gmail.com>
2024-07-15 11:33:45 +0530

    SAH-1536: Fix the issue in tests.

--- tests/resources/test_harness/central_node_mid.py
+++ tests/resources/test_harness/central_node_mid.py
@@ -90,6 +90,7 @@ class CentralNodeWrapperMid(CentralNodeWrapper):
             dish_fqdn063 = REAL_DISH63_FQDN
             dish_fqdn100 = REAL_DISH100_FQDN
 
+<<<<<<< HEAD
             # creating spfrx device fqdn
             self.spfrx_fqdn = dish_fqdn001.replace(
                 "mid-dish/dish-manager/SKA001",
@@ -100,6 +101,16 @@ class CentralNodeWrapperMid(CentralNodeWrapper):
             # Create Dish1 admin device proxy
             spfrx1_admin_dev_name = spfrx_proxy.adm_name()
             self.spfrx1_admin_dev_proxy = DeviceProxy(spfrx1_admin_dev_name)
+=======
+            LOGGER.info("Dish Manager 1 FQDN is: %s: ", dish_fqdn001)
+            spfrx_fqdn = dish_fqdn001.replace(
+                "mid-dish/dish-manager/SKA001",
+                "mid-dish/simulator-spfrx/SKA001",
+            )
+            LOGGER.info("spfrx_fqdn 1 FQDN is: %s: ", spfrx_fqdn)
+            spfrx_proxy = DeviceProxy(spfrx_fqdn)
+            LOGGER.info("spfrx_proxy 1 FQDN is: %s: ", spfrx_proxy)
+>>>>>>> 2f62b3dd (SAH-1536: Fix the issue in tests.)
 
             # Create database object for TMC TANGO DB
             self.db = Database()
