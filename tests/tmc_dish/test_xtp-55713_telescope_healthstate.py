875dd80aa63feb1ec02279275c0e286c310b578e
JayantKumbhar <jayant.ska@gmail.com>
2024-07-16 17:48:44 +0530

    SAH-1536: Resolve review comments

--- tests/tmc_dish/test_xtp-55713_telescope_healthstate.py
+++ tests/tmc_dish/test_xtp-55713_telescope_healthstate.py
@@ -3,12 +3,9 @@ import logging
 import time
 
 import pytest
-from pytest_bdd import given, parsers, scenario, then, when
+from pytest_bdd import parsers, scenario, then, when
 from ska_tango_base.control_model import HealthState
-from tango import DevState
 from tango.db import DbDevInfo
-
-from tests.resources.test_support.enum import DishMode
 
 
 @pytest.mark.tmc_dish
@@ -21,73 +18,6 @@ def test_tmc_TMC_healthstate():
     Test case verifying TMC TelescopeHealthState transition based on DISH-LMC
     subsystems HealthState
     """
-
-
-@given("a Telescope consisting of TMC, DISH, simulated CSP and simulated SDP")
-def given_a_telescope(central_node_mid):
-    """Given a TMC
-
-    Args:
-        central_node_mid : A fixture for CentralNode tango device class
-    """
-    assert central_node_mid.csp_master.ping() > 0
-    assert central_node_mid.sdp_master.ping() > 0
-    for dish_id in ["SKA001", "SKA036", "SKA063", "SKA100"]:
-        assert central_node_mid.dish_master_dict[dish_id].ping() > 0
-        assert central_node_mid.dish_leaf_node_dict[dish_id].ping() > 0
-
-
-@given("the Telescope is in ON state")
-def turn_on_telescope(central_node_mid, event_recorder):
-    """A method to put Telescope ON
-
-    Args:
-        central_node_mid : A fixture for CentralNode tango device class
-        event_recorder: A fixture for EventRecorder class_
-    """
-    central_node_mid.move_to_on()
-    event_recorder.subscribe_event(central_node_mid.csp_master, "State")
-    event_recorder.subscribe_event(central_node_mid.sdp_master, "State")
-
-    assert event_recorder.has_change_event_occurred(
-        central_node_mid.csp_master,
-        "State",
-        DevState.ON,
-    )
-    assert event_recorder.has_change_event_occurred(
-        central_node_mid.sdp_master,
-        "State",
-        DevState.ON,
-    )
-
-    for dish_id in ["SKA001", "SKA036", "SKA063", "SKA100"]:
-        event_recorder.subscribe_event(
-            central_node_mid.dish_master_dict[dish_id], "dishMode"
-        )
-        event_recorder.subscribe_event(
-            central_node_mid.dish_leaf_node_dict[dish_id], "dishMode"
-        )
-
-        assert event_recorder.has_change_event_occurred(
-            central_node_mid.dish_master_dict[dish_id],
-            "dishMode",
-            DishMode.STANDBY_FP,
-        )
-        assert event_recorder.has_change_event_occurred(
-            central_node_mid.dish_leaf_node_dict[dish_id],
-            "dishMode",
-            DishMode.STANDBY_FP,
-        )
-
-    event_recorder.subscribe_event(
-        central_node_mid.central_node, "telescopeState"
-    )
-
-    assert event_recorder.has_change_event_occurred(
-        central_node_mid.central_node,
-        "telescopeState",
-        DevState.ON,
-    )
 
 
 @when(parsers.parse("the {device} health state changes to {health_state}"))
