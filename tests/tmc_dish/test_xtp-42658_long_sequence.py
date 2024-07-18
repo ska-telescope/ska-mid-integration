72e622df6f8b1ec428e77d4a222667cfa902a73c
JayantKumbhar <jayant.ska@gmail.com>
2024-07-18 15:04:06 +0530

    SAH-1536: Test pipeline

--- tests/tmc_dish/test_xtp-42658_long_sequence.py
+++ tests/tmc_dish/test_xtp-42658_long_sequence.py
@@ -143,6 +143,19 @@ def configure_subarray(
             PointingState.TRACK,
             lookahead=10,
         )
+        logging.info(
+            "longRunningCommandResult for DishLN after configure1 %s",
+            central_node_mid.dish_leaf_node_dict[
+                dish_id
+            ].longRunningCommandResult,
+        )
+        logging.info("pytest.command_result1: %s", str(pytest.command_result))
+        assert event_recorder.has_change_event_occurred(
+            central_node_mid.dish_leaf_node_dict[dish_id],
+            "longRunningCommandResult",
+            (pytest.command_result[1][0], str(ResultCode.OK.value)),
+            lookahead=15,
+        )
     assert event_recorder.has_change_event_occurred(
         subarray_node.subarray_node,
         "obsState",
@@ -266,6 +279,19 @@ def reconfigure_subarray(
             central_node_mid.dish_leaf_node_dict[dish_id],
             "pointingState",
             PointingState.TRACK,
+            lookahead=15,
+        )
+        logging.info(
+            "longRunningCommandResult for DishLN after configure2 %s",
+            central_node_mid.dish_leaf_node_dict[
+                dish_id
+            ].longRunningCommandResult,
+        )
+        logging.info("pytest.command_result2: %s", str(pytest.command_result))
+        assert event_recorder.has_change_event_occurred(
+            central_node_mid.dish_leaf_node_dict[dish_id],
+            "longRunningCommandResult",
+            (pytest.command_result[1][0], str(ResultCode.OK.value)),
             lookahead=15,
         )
     assert event_recorder.has_change_event_occurred(
