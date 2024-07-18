e533f9f6e53ad6b68023a2473a1370def14dbe3c
shraddhabajare <shraddhab.ska@gmail.com>
2024-07-18 17:56:01 +0530

    SAH-1561: Remove unused imports

--- tests/invalid_json_tests/test_configure_invalid_json.py
+++ tests/invalid_json_tests/test_configure_invalid_json.py
@@ -8,7 +8,6 @@ from tango import DeviceProxy, EventType
 
 from tests.conftest import LOGGER
 from tests.resources.test_harness.constant import COMMAND_COMPLETED
-from tests.resources.test_support.common_utils.result_code import ResultCode
 from tests.resources.test_support.common_utils.telescope_controls import (
     BaseTelescopeControl,
 )
