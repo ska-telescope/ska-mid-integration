6c4cfc7786b4c6c53dc17b26759ce8a1de62e041
apurvapatkar <apurva.ska@gmail.com>
2024-07-19 10:54:33 +0530

    SAH-1558: Resolve review comments.

--- tests/resources/test_harness/subarray_node.py
+++ tests/resources/test_harness/subarray_node.py
@@ -56,6 +56,7 @@ from tests.resources.test_harness.utils.sync_decorators import (
     sync_release_resources,
     sync_restart,
 )
+from tests.resources.test_harness.utils.wait_helpers import Waiter
 from tests.resources.test_support.common_utils.common_helpers import Resource
 
 configure_logging(logging.DEBUG)
@@ -437,7 +438,8 @@ class SubarrayNodeWrapper(object):
             # Waiting for few seconds as the SubarrayNode End command
             # completion does not consider Dishes pointingState transition
             # to READY
-            time.sleep(2)
+            the_waiter = Waiter()
+            the_waiter.wait(80)
 
             self.abort_subarray()
             self.restart_subarray()
