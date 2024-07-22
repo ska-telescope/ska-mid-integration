485236c690623f76d03379c0396ef380b3085bca
manthan <manthan.ska@gmail.com>
2024-07-22 15:40:38 +0530

    SAH-1564: Update test case

--- tests/tmc_dish/test_xtp-29416_configure.py
+++ tests/tmc_dish/test_xtp-29416_configure.py
@@ -156,8 +156,6 @@ def check_dish_mode_and_pointing_state(
         dish_ids (str): Comma-separated IDs of DISH components.
     """
     for dish_id in dish_ids.split(","):
-        dish_client = central_node_mid.dish_master_dict[dish_id]
-        dish_client.Slew([181.235672347698, 30.309299188458])
         event_recorder.subscribe_event(
             central_node_mid.dish_master_dict[dish_id], "pointingState"
         )
@@ -176,6 +174,8 @@ def check_dish_mode_and_pointing_state(
             DishMode.OPERATE,
             lookahead=10,
         )
+        dish_client = central_node_mid.dish_master_dict[dish_id]
+        dish_client.Slew([181.235672347698, 30.309299188458])
         assert event_recorder.has_change_event_occurred(
             central_node_mid.dish_master_dict[dish_id],
             "pointingState",
