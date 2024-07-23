385aabe30fb18695953893bf567d1fe0f63ceda6
manthan <manthan.ska@gmail.com>
2024-07-23 13:00:35 +0530

    SAH-1564: Update test case

--- tests/tmc_dish/test_xtp-29416_configure.py
+++ tests/tmc_dish/test_xtp-29416_configure.py
@@ -2,6 +2,7 @@
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 
 import json
@@ -9,6 +10,10 @@ import json
 >>>>>>> 38ac12a5 (SAH-1564: Revert change)
 =======
 >>>>>>> b5ed1ca7 (SAH-1564: Revert change)
+=======
+import json
+
+>>>>>>> 9bc2f1ff (SAH-1564: Update test case)
 import pytest
 from pytest_bdd import given, parsers, scenario, then, when
 from ska_tango_base.control_model import ObsState
@@ -229,12 +234,19 @@ def invoke_configure(
     event_recorder.subscribe_event(
         subarray_node.subarray_node, "longRunningCommandResult"
     )
+<<<<<<< HEAD
+=======
+    dl = central_node_mid.dish_master_dict["001"]
+>>>>>>> 9bc2f1ff (SAH-1564: Update test case)
     configure_input_json = prepare_json_args_for_commands(
         "configure_mid", command_input_factory
     )
+    config_json = json.loads(configure_input_json)
+    config_json["pointing"]["target"]["ra"] = json.loads(dl.actual_pointing)[1]
+    config_json["pointing"]["target"]["dec"] = json.loads(dl.actualPointing)[2]
     central_node_mid.set_subarray_id(subarray_id)
     pytest.command_result = subarray_node.execute_transition(
-        "Configure", configure_input_json
+        "Configure", json.dumps(config_json)
     )
 
 
