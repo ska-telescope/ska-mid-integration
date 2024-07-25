258530a3b9721c1fb4f465ed640d26251d76a7e4
manthan <manthan.ska@gmail.com>
2024-07-25 11:47:17 +0530

    SAH-1564: Test pipline

--- tests/tmc_dish/test_xtp-42658_long_sequence.py
+++ tests/tmc_dish/test_xtp-42658_long_sequence.py
@@ -47,7 +47,11 @@ LOGGER = logging.getLogger(__name__)
 =======
 >>>>>>> 2ee3ddf7 (SAH-1536: Enable skipped tmc-dish tests)
 
+<<<<<<< HEAD
 @pytest.mark.skip(reason="Dish pointingstate issue")
+=======
+@pytest.mark.skip
+>>>>>>> cbe762ab (SAH-1564: Test pipline)
 @pytest.mark.tmc_dish
 @scenario(
     "../features/tmc_dish/xtp-42658_long_sequence.feature",
