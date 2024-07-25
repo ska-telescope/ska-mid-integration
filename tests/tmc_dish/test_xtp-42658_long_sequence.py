26c546e77ad297a3ae83dbf89fe3a79b4ffaada7
manthan <manthan.ska@gmail.com>
2024-07-25 18:32:32 +0530

    SAH-1564: Update test case

--- tests/tmc_dish/test_xtp-42658_long_sequence.py
+++ tests/tmc_dish/test_xtp-42658_long_sequence.py
@@ -110,8 +110,6 @@ def configure_subarray(
     configure_input_json = json.loads(input_json)
     configure_input_json["dish"]["receiver_band"] = receiver_band_1
     configure_input_json["csp"]["common"]["frequency_band"] = "1"
-    configure_input_json["pointing"]["target"]["ra"] = "21:10:47.92"
-    configure_input_json["pointing"]["target"]["dec"] = "-88:55:22.9"
     central_node_mid.set_subarray_id(subarray_id)
     pytest.command_result = subarray_node.store_configuration_data(
         json.dumps(configure_input_json)
@@ -236,6 +234,8 @@ def reconfigure_subarray(
     configure_input_json = json.loads(input_json)
     configure_input_json["dish"]["receiver_band"] = receiver_band_2
     configure_input_json["csp"]["common"]["frequency_band"] = "2"
+    configure_input_json["pointing"]["target"]["ra"] = "25:10:47.92"
+    configure_input_json["pointing"]["target"]["dec"] = "-80:55:22.9"
     central_node_mid.set_subarray_id(subarray_id)
     pytest.command_result = subarray_node.execute_transition(
         "Configure", json.dumps(configure_input_json)
