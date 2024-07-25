f68ddbb79f865140ff1b17cc104f1c7ee1fe325d
manthan <manthan.ska@gmail.com>
2024-07-25 18:48:21 +0530

    SAH-1564: Use dev image of dish

--- tests/tmc_dish/test_xtp-42658_long_sequence.py
+++ tests/tmc_dish/test_xtp-42658_long_sequence.py
@@ -234,8 +234,6 @@ def reconfigure_subarray(
     configure_input_json = json.loads(input_json)
     configure_input_json["dish"]["receiver_band"] = receiver_band_2
     configure_input_json["csp"]["common"]["frequency_band"] = "2"
-    configure_input_json["pointing"]["target"]["ra"] = "25:10:47.92"
-    configure_input_json["pointing"]["target"]["dec"] = "-80:55:22.9"
     central_node_mid.set_subarray_id(subarray_id)
     pytest.command_result = subarray_node.execute_transition(
         "Configure", json.dumps(configure_input_json)
