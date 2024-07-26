4afefb331932f3a731a3d7596fa55abcce67d164
mesnehal <snehal.ujjainkar@skao.int>
2024-07-26 16:34:25 +0100

    SSt: Disable test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,12 @@
 # pylint: disable=unused-argument
 
-import tango # type: ignore
+# import tango # type: ignore
 
 
-def pytest_sessionstart(session):
-    """
-    Pytest hook; prints info about tango version.
-    :param session: a pytest Session object
-    :type session: :py:class:`pytest.Session`
-    """
-    print(tango.utils.info())
+# def pytest_sessionstart(session):
+#     """
+#     Pytest hook; prints info about tango version.
+#     :param session: a pytest Session object
+#     :type session: :py:class:`pytest.Session`
+#     """
+#     print(tango.utils.info())
