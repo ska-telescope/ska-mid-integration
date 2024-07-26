d03d880e8937437cb6bd6476319ecdf65f8d67d7
mesnehal <snehal.ujjainkar@skao.int>
2024-07-26 16:03:55 +0100

    SST: Update tests to run in pipeline

---
+++ tests/conftest.py
@@ -0,0 +1,13 @@
+# pylint: disable=unused-argument
+
+import pytest
+import tango
+
+
+def pytest_sessionstart(session):
+    """
+    Pytest hook; prints info about tango version.
+    :param session: a pytest Session object
+    :type session: :py:class:`pytest.Session`
+    """
+    print(tango.utils.info())
