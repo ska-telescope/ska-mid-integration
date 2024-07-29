41d1649d83aca3d50ff5ef710c323f98d4990389
manthan <manthan.ska@gmail.com>
2024-07-29 19:24:16 +0530

    SAH-1564: Set seprate CPU request for dishleafnode

--- tests/invalid_json_tests/test_configure_invalid_json.py
+++ tests/invalid_json_tests/test_configure_invalid_json.py
@@ -30,7 +30,6 @@ tmc_helper = TmcHelper(centralnode, tmc_subarraynode1)
 telescope_control = BaseTelescopeControl()
 
 
-@pytest.mark.hope
 @pytest.mark.SKA_mid
 @scenario(
     "../features/check_invalid_json_not_allowed.feature",
