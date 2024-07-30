13b3ae43731a03ebf1ae24752c8727017c41b5f1
mansi.shirore <mansis.ska@gmail.com>
2024-07-30 16:40:33 +0530

    SAH-1567: Add MArker

--- tests/tmc_harness_testcases/bdd_scenarios/test_xtp_49324_csp_error_propogation.py
+++ tests/tmc_harness_testcases/bdd_scenarios/test_xtp_49324_csp_error_propogation.py
@@ -16,6 +16,7 @@ from tests.resources.test_harness.utils.common_utils import JsonFactory
 from tests.resources.test_harness.utils.enums import SimulatorDeviceType
 
 
+@pytest.mark.csppropagation
 @pytest.mark.SKA_mid
 @scenario(
     "../features/test_harness/xtp_49324_csp_subarray_"
