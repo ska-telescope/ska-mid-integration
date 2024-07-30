0f81f2512783fe840faa1c136a33c075540db791
JayantKumbhar <jayant.ska@gmail.com>
2024-07-30 23:55:44 +0530

    SAH-1564: Disable tmc-dish end() test case

--- tests/tmc_dish/test_xtp-29417_end.py
+++ tests/tmc_dish/test_xtp-29417_end.py
@@ -211,8 +211,7 @@ def invoke_end(central_node_mid, subarray_node, subarray_id, event_recorder):
     event_recorder.subscribe_event(
         subarray_node.subarray_node, "longRunningCommandResult"
     )
-    # pytest.command_result = subarray_node.execute_transition("End")
-    pytest.command_result = subarray_node.subarray_node.End()
+    pytest.command_result = subarray_node.execute_transition("End")
 
 
 @then(
