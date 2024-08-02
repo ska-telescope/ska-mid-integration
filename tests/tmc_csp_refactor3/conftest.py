0b1422e6bc42b3b5137294cca28542eb72549a7e
Emanuele Lena <emanuele.lena@designcoaching.net>
2024-08-02 09:51:40 +0200

    OMCPT-39: remove dummy info

--- tests/tmc_csp_refactor3/conftest.py
+++ tests/tmc_csp_refactor3/conftest.py
@@ -1,9 +1,5 @@
 """Configurations needed for the tests using the new harness."""
 
-
-import logging
-
-# Define a fixture to store things, like the starting state
 from dataclasses import dataclass
 from typing import Any
 
@@ -14,7 +10,6 @@ from ska_integration_test_harness.common_utils.typed_logger import log_events
 from ska_integration_test_harness.common_utils.typed_tracer import (
     TypedTangoEventTracer,
 )
-from ska_integration_test_harness.dummy import dummy_version
 from ska_integration_test_harness.facades.csp_facade import CSPFacade
 from ska_integration_test_harness.facades.dishes_facade import DishesFacade
 from ska_integration_test_harness.facades.sdp_facade import SDPFacade
@@ -64,7 +59,7 @@ def telescope_wrapper(
     default_commands_inputs: ObsStateCommandsInput,
 ) -> TelescopeWrapper:
     """Create an unique test harness with proxies to all devices."""
-    logging.info("Test harness verision: %s", dummy_version())
+    # logging.info("Test harness verision: %s", dummy_version())
     components_factory = TelescopeStructureFactory(
         default_commands_inputs, DEFAULT_VCC_CONFIG_INPUT
     )
