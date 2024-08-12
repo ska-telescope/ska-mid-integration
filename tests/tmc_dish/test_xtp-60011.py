9d218a075e46959e6bf282ad074a517246297b0b
Muskan Sharma <muskans.ska@gmail.com>
2024-08-13 00:02:00 +0530

    HM-521: Execute test cases.

--- tests/tmc_dish/test_xtp-60011.py
+++ tests/tmc_dish/test_xtp-60011.py
@@ -225,7 +225,7 @@ def check_subarray_obsState_ready(
         subarray_node.subarray_node, "obsState", ObsState.READY, lookahead=10
     )
     event_recorder.subscribe_event(
-        central_node_mid.central_node, "longRunningCommandResult"
+        subarray_node.subarray_node, "longRunningCommandResult"
     )
     assert event_recorder.has_change_event_occurred(
         subarray_node.subarray_node,
