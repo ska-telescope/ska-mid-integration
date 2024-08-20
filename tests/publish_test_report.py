f1f4b6ac1fdcc19a44056b2990683b9667d0b9a5
Emanuele Lena <emanuele.lena@designcoaching.net>
2024-08-20 10:20:24 +0200

    OMCPT-61: try to search for the created JIRA execution issue (debugging - change link to v2 API)

--- tests/publish_test_report.py
+++ tests/publish_test_report.py
@@ -11,6 +11,11 @@ JIRA_AUTH = os.getenv("JIRA_AUTH")
 PROJECT_KEY = "XTP"
 
 
+def auth_header() -> dict:
+    """Return the authorization header for JIRA API."""
+    return {"Authorization": f"Basic {JIRA_AUTH}"}
+
+
 def print_env_vars() -> str:
     """Print the environment variables for debugging."""
     msg = f"CI_JOB_URL: {CI_JOB_URL}, JIRA_URL: {JIRA_URL}, "
@@ -30,14 +35,12 @@ def search_jira_execution_issue() -> str:
         f'AND description ~ "{CI_JOB_URL}"'
     )
 
-    headers = {"Authorization": f"Basic {JIRA_AUTH}"}
-
-    search_api_path = "/rest/api/3/search"
+    search_api_path = "/rest/api/2/search"
     search_api_url = f"{JIRA_URL}{search_api_path}"
 
     # Perform the search
     response = requests.get(
-        search_api_url, headers=headers, params={"jql": query}
+        search_api_url, headers=auth_header(), params={"jql": query}
     )
 
     # Check the response status
@@ -45,22 +48,53 @@ def search_jira_execution_issue() -> str:
         issues = response.json().get("issues", [])
         if issues:
             for issue in issues:
-                msg = f'Found Test Execution Ticket: {issue["key"]}'
-                msg += f'Summary: {issue["fields"]["summary"]}'
-                msg += f'Description: {issue["fields"]["description"]}'
-                msg += f'URL: {JIRA_URL}/browse/{issue["key"]}'
+                msg = f'Found Test Execution Ticket: {issue["key"]}.\n'
+                msg += f'Summary: {issue["fields"]["summary"]}\n'
+                msg += f'Description: {issue["fields"]["description"]}\n'
+                msg += f'URL: {JIRA_URL}/browse/{issue["key"]}\n'
                 logging.info(msg)
         else:
             logging.warning("No matching test execution tickets found.")
     else:
-        msg = "Failed to search JIRA tickets, "
-        msg += f"status code: {response.status_code}"
-        msg += f"URL: {search_api_url}"
-        msg += f"Query: {query}"
-        msg += f"Response: {response.text}"
+        msg = "Failed to search JIRA tickets:\n"
+        msg += f"status code: {response.status_code}\n"
+        msg += f"URL: {search_api_url}\n"
+        msg += f"Query: {query}\n"
+        msg += f"Response: {response.text}\n"
         logging.error(msg)
 
     return issues
+
+
+def publish_test_report_on_issue(issue) -> str:
+
+    issue_ticket = issue["key"]
+
+    update_api_path = f"/rest/api/2/issue/{issue_ticket}"
+    update_api_url = f"{JIRA_URL}{update_api_path}"
+
+    new_description = issue["fields"]["description"]
+    new_description += f"Hello world! This is a test update for {issue_ticket}"
+
+    update_payload = {
+        "fields": {
+            "description": new_description,
+        }
+    }
+
+    response = requests.put(
+        update_api_url, headers=auth_header(), json=update_payload
+    )
+
+    if response.status_code == 204:
+        logging.info(f"Updated description for {issue_ticket}")
+    else:
+        msg = "Failed to update JIRA ticket:\n"
+        msg += f"status code: {response.status_code}\n"
+        msg += f"URL: {update_api_url}\n"
+        msg += f"Payload: {update_payload}\n"
+        msg += f"Response: {response.text}\n"
+        logging.error(msg)
 
 
 if __name__ == "__main__":
