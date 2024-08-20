e8a6cf3dbf86f12fe1c5c2c635c3e1d29c652d69
Emanuele Lena <emanuele.lena@designcoaching.net>
2024-08-20 09:59:29 +0200

    OMCPT-61: try to search for the created JIRA execution issue (lint and format)

--- tests/publish_test_report.py
+++ tests/publish_test_report.py
@@ -1,52 +1,48 @@
-
+"""Publish HTML BDD test report to JIRA Test Execution issue."""
 
 import logging
 import os
 
 import requests
 
-CI_JOB_URL = os.getenv('CI_JOB_URL')
-JIRA_URL = os.getenv('JIRA_URL')
-JIRA_AUTH = os.getenv('JIRA_AUTH')
+CI_JOB_URL = os.getenv("CI_JOB_URL")
+JIRA_URL = os.getenv("JIRA_URL")
+JIRA_AUTH = os.getenv("JIRA_AUTH")
 PROJECT_KEY = "XTP"
 
+
 def print_env_vars() -> str:
-    return (
-        f'CI_JOB_URL: {CI_JOB_URL}, JIRA_URL: {JIRA_URL},
-          JIRA_AUTH: {JIRA_AUTH}, PROJECT_KEY: {PROJECT_KEY}'
-        )
+    """Print the environment variables for debugging."""
+    msg = f"CI_JOB_URL: {CI_JOB_URL}, JIRA_URL: {JIRA_URL}, "
+    msg += f'JIRA_AUTH: {"[something]" if JIRA_AUTH else "[None]"}, '
+    msg += f"PROJECT_KEY: {PROJECT_KEY}"
+    return msg
 
 
 def search_jira_execution_issue() -> str:
     """Search for the Jira issue that corresponds to the current CI job."""
     if not CI_JOB_URL or not JIRA_URL or not JIRA_AUTH or not PROJECT_KEY:
-        raise ValueError(
-            'Missing environment variables: '
-            + print_env_vars()
-        )
-    
+        raise ValueError("Missing environment variables: " + print_env_vars())
+
     query = (
-            f'project = "{PROJECT_KEY}" '
-            'AND issuetype = "TestExecution" '
-            f'AND description ~ "{CI_JOB_URL}"'
-        )
-    
+        f'project = "{PROJECT_KEY}" '
+        'AND issuetype = "TestExecution" '
+        f'AND description ~ "{CI_JOB_URL}"'
+    )
+
     headers = {"Authorization": f"Basic {JIRA_AUTH}"}
 
-    
-    search_api_path = '/rest/api/3/search'
-    search_api_url = f'{JIRA_URL}{search_api_path}'
+    search_api_path = "/rest/api/3/search"
+    search_api_url = f"{JIRA_URL}{search_api_path}"
 
     # Perform the search
     response = requests.get(
-        search_api_url,
-        headers=headers,
-        params={'jql': query}
+        search_api_url, headers=headers, params={"jql": query}
     )
 
     # Check the response status
     if response.status_code == 200:
-        issues = response.json().get('issues', [])
+        issues = response.json().get("issues", [])
         if issues:
             for issue in issues:
                 msg = f'Found Test Execution Ticket: {issue["key"]}'
@@ -57,26 +53,16 @@ def search_jira_execution_issue() -> str:
         else:
             logging.warning("No matching test execution tickets found.")
     else:
-        msg = f'Failed to search JIRA tickets, status code: {response.status_code}'
-        msg += f'URL: {search_api_url}'
-        msg += f'Query: {query}'
-        msg += f'Response: {response.text}'
+        msg = "Failed to search JIRA tickets, "
+        msg += f"status code: {response.status_code}"
+        msg += f"URL: {search_api_url}"
+        msg += f"Query: {query}"
+        msg += f"Response: {response.text}"
         logging.error(msg)
 
     return issues
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     search_jira_execution_issue()
-
-
-
-    
-
-
-
-    
-
-    
-
