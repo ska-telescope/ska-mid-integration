9d124352a93adf28f1d6b605d6e7e2f529ca5a7e
Muskan Sharma <muskans.ska@gmail.com>
2024-08-22 16:09:33 +0530

    HM-521: Update submodule.

--- tests/tmc_harness_testcases/bdd_scenarios/test_xtp-60011.py
+++ tests/tmc_harness_testcases/bdd_scenarios/test_xtp-60011.py
@@ -29,6 +29,7 @@ TIMEOUT = 110
 
 
 @pytest.mark.SKA_mid
+@pytest.mark.test1
 @scenario(
     "../features/test_harness/xtp-60011_configure_with_correction_key.feature",
     "TMC Behavior During correction key handling",
