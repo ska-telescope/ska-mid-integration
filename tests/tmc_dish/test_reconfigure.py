41c67f7a968d3d7d2d92f2d76f8a78b4c1170a01
SKAIN <truptin.ska@gmail.com>
2024-10-01 22:22:11 +0530

    SAH-1596:Test Case clean up

--- tests/tmc_dish/test_reconfigure.py
+++ tests/tmc_dish/test_reconfigure.py
@@ -16,7 +16,7 @@ telescope_control = BaseTelescopeControl()
 )
 def test_multiple_configure_functionality():
     """
-    Test TMC allows multiple configuration
+    Test TMC allows reconfiguration
 
     """
 
@@ -28,17 +28,7 @@ def send_configure(json_factory, input_json1, subarray_node):
     _, pytest.unique_id = subarray_node.execute_transition(
         "Configure", configure_json1
     )
-    LOGGER.info("Configure1 is invoked successfully")
-
-
-# @then("the subarray transitions to obsState READY")
-# def check_for_ready(subarray_node, event_recorder):
-#     # Verify ObsState is READY
-#     LOGGER.info("Verifying obsState READY after Configure1")
-#     wait_for_device_status_ready(subarray_node.subarray_node)
-#     assert event_recorder.has_change_event_occurred(
-#         subarray_node.subarray_node, "obsState", ObsState.READY, lookahead=20
-#     )
+    LOGGER.info("Configure-1 is invoked successfully")
 
 
 @when(
@@ -49,20 +39,9 @@ def send_configure(json_factory, input_json1, subarray_node):
 def send_next_configure(
     json_factory, input_json2, subarray_node, event_recorder
 ):
-    configure_json1 = json_factory(input_json2)
+    configure_json = json_factory(input_json2)
     LOGGER.info("Invoking Configure command with input_json1")
     _, pytest.unique_id = subarray_node.execute_transition(
-        "Configure", configure_json1
+        "Configure", configure_json
     )
-    LOGGER.info("Configure1 is invoked successfully")
-
-
-# @then("the subarray reconfigures changing its obsState to READY")
-# def check_for_reconfigure_ready(subarray_node, event_recorder):
-#
-#     LOGGER.info("Verifying obsState READY after Configure2")
-#
-#     wait_for_device_status_ready(subarray_node.subarray_node)
-#     assert event_recorder.has_change_event_occurred(
-#         subarray_node.subarray_node, "obsState", ObsState.READY, lookahead=20
-#     )
+    LOGGER.info("Configure-2 is invoked successfully")
