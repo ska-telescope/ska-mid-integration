156d947e28fcbd42cafa3e36617385311c2ccda0
SKAIN <truptin.ska@gmail.com>
2024-10-01 08:46:33 +0530

    SAH-1596:Updated test

--- tests/tmc_dish/test_reconfigure.py
+++ tests/tmc_dish/test_reconfigure.py
@@ -122,15 +122,13 @@ def send_configure(json_factory, input_json1):
     try:
         LOGGER.info("Invoking Configure command with input_json1")
         # Invoke Configure() command
-        result, message = tmc_helper.configure_subarray(
+        tmc_helper.configure_subarray(
             configure_json1, **ON_OFF_DEVICE_COMMAND_DICT
         )
-        LOGGER.info(
-            "Configure1 is invoked successfully %s %s", result, message
-        )
+        LOGGER.info("Configure1 is invoked successfully")
 
     except Exception as e:
-        LOGGER.info("Exception raised %s %s %s ", e, result, message)
+        LOGGER.info("Exception raised %s ", e)
         tear_down(release_json, **ON_OFF_DEVICE_COMMAND_DICT)
 
 
