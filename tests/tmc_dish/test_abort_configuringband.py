620481a7f4bcc386022e4ab0f1a071930dffd29f
JayantKumbhar <jayant.ska@gmail.com>
2024-10-04 15:01:16 +0530

    SAH-1588: Abort dish during setoperate mode

--- tests/tmc_dish/test_abort_configuringband.py
+++ tests/tmc_dish/test_abort_configuringband.py
@@ -211,7 +211,7 @@ def check_dish_mode_and_pointing_state(
         assert event_recorder.has_change_event_occurred(
             central_node_mid.dish_master_dict[dish_id],
             "longRunningCommandResult",
-            (Anything, '[7, "ConfigureBand1 Aborted"]'),
+            (Anything, '[7, "SetOperateMode Aborted"]'),
             lookahead=10,
         )
 
