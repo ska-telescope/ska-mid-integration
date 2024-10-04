4f76f5e54e6a22fe37eca7241b390009b74af447
mansi.shirore <mansis.ska@gmail.com>
2024-10-04 17:54:31 +0530

    SAH-1597: Disable test case

--- tests/tmc_csp/test_delay_model.py
+++ tests/tmc_csp/test_delay_model.py
@@ -22,6 +22,7 @@ from tests.resources.test_harness.utils.common_utils import JsonFactory
 LOGGER = logging.getLogger(__name__)
 
 
+@pytest.mark.skip
 @pytest.mark.tmc_csp
 @scenario(
     "../features/test_harness/xtp_35778_test_delay_model.feature",
