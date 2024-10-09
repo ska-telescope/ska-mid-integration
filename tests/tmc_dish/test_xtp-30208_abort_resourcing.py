1211d3e948de0db2970e6b11517152f4eb71c669
mansi.shirore <mansis.ska@gmail.com>
2024-10-09 10:28:29 +0530

    SAH-1597: Disable dish test case

--- tests/tmc_dish/test_xtp-30208_abort_resourcing.py
+++ tests/tmc_dish/test_xtp-30208_abort_resourcing.py
@@ -11,6 +11,7 @@ from tests.resources.test_harness.helpers import (
 from tests.resources.test_support.enum import DishMode
 
 
+@pytest.mark.skip
 @pytest.mark.tmc_dish
 @scenario(
     "../features/tmc_dish/xtp-30208_abort_resourcing.feature",
