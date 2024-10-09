2676af597c54729b788e64d9dec864166d46fb84
mansi.shirore <mansis.ska@gmail.com>
2024-10-09 16:10:16 +0530

    SAH-1597: Disable test cases

--- tests/tmc_csp/test_xtp_40176_long_sequence_configure_scan_scan.py
+++ tests/tmc_csp/test_xtp_40176_long_sequence_configure_scan_scan.py
@@ -21,6 +21,7 @@ configure_logging(logging.DEBUG)
 LOGGER = logging.getLogger(__name__)
 
 
+@pytest.mark.skip(reason="issue at CSP side for configure")
 @pytest.mark.tmc_csp
 @scenario(
     "../features/tmc_csp/xtp_40175_long_sequence_configure_scan.feature",
