8232856d9392d6868678bfa1eb8350ee3da516e4
mansi.shirore <mansis.ska@gmail.com>
2024-10-14 14:10:37 +0530

    SAH-1597: Enable test case

--- tests/tmc_dish/test_xtp-42757_successive_scan.py
+++ tests/tmc_dish/test_xtp-42757_successive_scan.py
@@ -18,7 +18,6 @@ from tests.resources.test_harness.helpers import (
 from tests.resources.test_support.enum import DishMode, PointingState
 
 
-@pytest.mark.skip
 @pytest.mark.tmc_dish
 @scenario(
     "../features/tmc_dish/xtp-42757_successive_scan.feature",
