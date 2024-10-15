4babe0733c4c5ad90acd38710ee76e338020dd3a
SKAIN <truptin.ska@gmail.com>
2024-10-15 18:59:48 +0530

    SAH-1596:SN image updated

--- tests/tmc_dish/test_xtp-42658_long_sequence.py
+++ tests/tmc_dish/test_xtp-42658_long_sequence.py
@@ -25,6 +25,7 @@ from tests.resources.test_harness.utils.common_utils import JsonFactory
 from tests.resources.test_support.enum import DishMode, PointingState
 
 
+@pytest.mark.skip(reason="Needs to be unskipped once SKB-606 is resolved")
 @pytest.mark.tmc_dish
 @scenario(
     "../features/tmc_dish/xtp-42658_long_sequence.feature",
--- tests/tmc_dish/test_xtp-64935_reconfigure.py
+++ tests/tmc_dish/test_xtp-64935_reconfigure.py
@@ -9,6 +9,7 @@ from tests.resources.test_support.common_utils.telescope_controls import (
 telescope_control = BaseTelescopeControl()
 
 
+@pytest.mark.skip(reason="Needs to be unskipped once SKB-606 is resolved")
 @pytest.mark.tmc_dish
 @scenario(
     "../features/tmc_dish/xtp-64935.feature",
