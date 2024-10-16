a8e6c10d1daae78f6def89e3fe18feedca50539a
JayantKumbhar <jayant.ska@gmail.com>
2024-10-16 11:13:24 +0530

    SAH-1588: Enable tmc-csp skipped pairwise tests

--- tests/tmc_csp/test_xtp_40175_long_sequance_configure_scan.py
+++ tests/tmc_csp/test_xtp_40175_long_sequance_configure_scan.py
@@ -21,7 +21,7 @@ configure_logging(logging.DEBUG)
 LOGGER = logging.getLogger(__name__)
 
 
-@pytest.mark.skip("It fails. TODO: why?")
+@pytest.mark.SKA_mid1
 @pytest.mark.tmc_csp
 @scenario(
     "../features/tmc_csp/xtp_40175_long_sequence_configure_scan.feature",
