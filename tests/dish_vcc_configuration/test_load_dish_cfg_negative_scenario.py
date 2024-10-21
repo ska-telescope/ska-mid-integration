d1286c7c9387e8b18e435bf1cb0cc6ee44be57b4
JayantKumbhar <jayant.ska@gmail.com>
2024-10-21 13:42:21 +0530

    SAH-1588: Test pipeline

--- tests/dish_vcc_configuration/test_load_dish_cfg_negative_scenario.py
+++ tests/dish_vcc_configuration/test_load_dish_cfg_negative_scenario.py
@@ -63,6 +63,9 @@ def test_central_node_return_error_for_duplicate_vcc_id():
 
 
 @pytest.mark.SKA_mid
+@pytest.mark.skip(
+    reason="This will be enable once Dish Vcc feature is integrate."
+)
 @scenario(
     "../features/load_dish_cfg_command_negative_scenario.feature",
     "TMC handling exception from CSP Subarray",
