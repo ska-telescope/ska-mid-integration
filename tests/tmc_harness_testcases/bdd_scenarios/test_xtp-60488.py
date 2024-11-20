5ccd7281ed252f7bcf0ceeabe636b7a7bcdd82df
apurvapatkar <apurva.ska@gmail.com>
2024-11-20 11:51:25 +0530

    SAH-1623: Execute tests.

--- tests/tmc_harness_testcases/bdd_scenarios/test_xtp-60488.py
+++ tests/tmc_harness_testcases/bdd_scenarios/test_xtp-60488.py
@@ -28,9 +28,9 @@ from tests.resources.test_harness.utils.enums import SimulatorDeviceType
 TIMEOUT = 110
 
 
-@pytest.mark.skip(
-    reason="Observed issue in Abort command while testing with simulators."
-)
+# @pytest.mark.skip(
+#     reason="Observed issue in Abort command while testing with simulators."
+# )
 @pytest.mark.SKA_mid
 @scenario(
     "../features/test_harness/xtp-60488_dish_correction_maintain_key.feature",
