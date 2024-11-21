933c7b0745c53440f7ac5b1024f6c30e8373aa1d
mansi.shirore <mansis.ska@gmail.com>
2024-11-21 10:33:11 +0530

    SAH-1581: Enable test cases

--- tests/tmc_csp/test_xtp-42551_long_sequence_with_end.py
+++ tests/tmc_csp/test_xtp-42551_long_sequence_with_end.py
@@ -22,7 +22,6 @@ configure_logging(logging.DEBUG)
 LOGGER = logging.getLogger(__name__)
 
 
-# @pytest.mark.skip(reason="issue at CSP for scan after configure (CIP-2967)")
 @pytest.mark.tmc_csp
 @scenario(
     "../features/tmc_csp/xtp_40175_long_sequence_configure_scan.feature",
