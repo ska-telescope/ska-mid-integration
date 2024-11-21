b6262f9aec77821a75c28d6f63f72fbbd463f01b
devesh.ska <devesh.ska@gmail.com>
2024-11-21 15:16:12 +0530

    SAH-1623: enable tests

--- tests/tmc_robustness/Configure_failure_handling_tests/test_configure_handling_on_sdp_idle_obsstate_failure.py
+++ tests/tmc_robustness/Configure_failure_handling_tests/test_configure_handling_on_sdp_idle_obsstate_failure.py
@@ -13,7 +13,6 @@ from tests.resources.test_harness.utils.enums import SimulatorDeviceType
 
 # The test fails intermittently as the test does not make sure that the Abort
 # is completed on dishes and then the next Configure is invoked
-@pytest.mark.skip(reason="Need to add dish assertions in the test.")
 @pytest.mark.SKA_mid
 @scenario(
     "../features/xtp-28835.feature",
