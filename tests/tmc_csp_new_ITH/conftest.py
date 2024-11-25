91b7bf0268b8471fd1d68af43a0a4d5a69cc07cf
Emanuele Lena <emanuele.lena@designcoaching.net>
2024-11-25 16:07:06 +0100

    SST-982: update ITH (remove a few strict checks on LRC)

--- tests/tmc_csp_new_ITH/conftest.py
+++ tests/tmc_csp_new_ITH/conftest.py
@@ -77,8 +77,7 @@ def telescope_wrapper(
 
     # build the wrapper of the telescope and it's sub-systems
     telescope = test_harness_builder.build()
-    telescope.actions_default_timeout = 120
-    telescope.tear_down()
+    telescope.actions_default_timeout = 60
     yield telescope
 
     # after a test is completed, reset the telescope to its initial state
