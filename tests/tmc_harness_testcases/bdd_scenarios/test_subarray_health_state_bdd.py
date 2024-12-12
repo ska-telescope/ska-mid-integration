e0f2a39ddef5eff62a214deec9ed529402f71a46
apurvapatkar <apurva.ska@gmail.com>
2024-12-12 17:06:23 +0530

    SAH-1630: Execute set of tests.

--- tests/tmc_harness_testcases/bdd_scenarios/test_subarray_health_state_bdd.py
+++ tests/tmc_harness_testcases/bdd_scenarios/test_subarray_health_state_bdd.py
@@ -12,6 +12,7 @@ from tests.resources.test_harness.helpers import (
 )
 
 
+@pytest.mark.sah1630
 @pytest.mark.SKA_mid
 @scenario(
     "../features/test_harness/subarray_health_state.feature",
@@ -25,6 +26,7 @@ def test_subarray_health_state_with_csp_and_sdp():
     """
 
 
+@pytest.mark.sah1630
 @pytest.mark.SKA_mid
 @scenario(
     "../features/test_harness/subarray_health_state.feature",
