ae76cce0d6f41bc04374d5cc8fe57b639d832157
apurvapatkar <apurva.ska@gmail.com>
2024-12-13 15:33:05 +0530

    SAH-1630: Execute tests.

--- tests/tmc_harness_testcases/bdd_scenarios/test_xtp_49354_assignresources_timeout_csp.py
+++ tests/tmc_harness_testcases/bdd_scenarios/test_xtp_49354_assignresources_timeout_csp.py
@@ -19,7 +19,7 @@ from tests.resources.test_harness.utils.common_utils import JsonFactory
 from tests.resources.test_harness.utils.enums import SimulatorDeviceType
 
 
-# @pytest.mark.skip(reason="TBD")
+@pytest.mark.skip(reason="TBD")
 @pytest.mark.SKA_mid
 @scenario(
     "../features/test_harness/"
