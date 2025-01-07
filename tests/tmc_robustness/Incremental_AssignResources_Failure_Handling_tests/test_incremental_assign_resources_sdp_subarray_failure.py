9fbdeedd62499d8deb6d69cd72b815c6fe24ca9c
devesh.ska <devesh.ska@gmail.com>
2025-01-07 16:57:42 +0530

    HM-585:  update test

--- tests/tmc_robustness/Incremental_AssignResources_Failure_Handling_tests/test_incremental_assign_resources_sdp_subarray_failure.py
+++ tests/tmc_robustness/Incremental_AssignResources_Failure_Handling_tests/test_incremental_assign_resources_sdp_subarray_failure.py
@@ -51,11 +51,11 @@ def given_tmc(central_node_mid, event_recorder):
     )
     event_recorder.subscribe_event(central_node_mid.subarray_node, "obsState")
     central_node_mid.move_to_on()
-    assert event_recorder.has_change_event_occurred(
-        central_node_mid.central_node,
-        "telescopeState",
-        DevState.ON,
-    )
+    # assert event_recorder.has_change_event_occurred(
+    #     central_node_mid.central_node,
+    #     "telescopeState",
+    #     DevState.ON,
+    # )
     assert event_recorder.has_change_event_occurred(
         central_node_mid.subarray_node,
         "obsState",
