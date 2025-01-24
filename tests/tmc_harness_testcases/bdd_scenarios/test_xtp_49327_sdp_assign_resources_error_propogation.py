3abe0690c353d7a4883cc123a54fc9b2e082efdf
manthan <manthan.ska@gmail.com>
2025-01-24 13:19:40 +0530

    SAH-1638: Run a single test case

--- tests/tmc_harness_testcases/bdd_scenarios/test_xtp_49327_sdp_assign_resources_error_propogation.py
+++ tests/tmc_harness_testcases/bdd_scenarios/test_xtp_49327_sdp_assign_resources_error_propogation.py
@@ -20,6 +20,7 @@ from tests.resources.test_harness.utils.common_utils import JsonFactory
 from tests.resources.test_harness.utils.enums import SimulatorDeviceType
 
 
+@pytest.mark.test1
 @pytest.mark.batch1
 @pytest.mark.SKA_mid
 @scenario(
