05391a92418234dfb24eb945c508e554c964d59d
SKAIN <truptin.ska@gmail.com>
2025-02-03 14:54:22 +0530

    SAH-1662:test configure sequence

--- tests/tmc_dish/test_xtp-29416_configure.py
+++ tests/tmc_dish/test_xtp-29416_configure.py
@@ -14,7 +14,6 @@ from tests.resources.test_harness.helpers import (
 from tests.resources.test_support.enum import DishMode, PointingState
 
 
-@pytest.mark.skip()
 @pytest.mark.tmc_dish
 @scenario(
     "../features/tmc_dish/xtp-29416_configure.feature",
