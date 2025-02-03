f9c8ca09c5d1a053aa8cca1ed84d8b82f1b3bd9d
SKAIN <truptin.ska@gmail.com>
2025-02-03 16:01:21 +0530

    SAH-1662:test abort in resourcing sequence

--- tests/tmc_dish/test_xtp-30208_abort_resourcing.py
+++ tests/tmc_dish/test_xtp-30208_abort_resourcing.py
@@ -11,7 +11,6 @@ from tests.resources.test_harness.helpers import (
 from tests.resources.test_support.enum import DishMode
 
 
-@pytest.mark.skip()
 @pytest.mark.tmc_dish
 @scenario(
     "../features/tmc_dish/xtp-30208_abort_resourcing.feature",
