4ff3c2cfbc3f858dcd179082a9e4079c1d0d4077
truptin.ska <truptin.ska@gmail.com>
2025-02-13 13:37:59 +0530

    SAH-1662:updated CN and SDP images

--- tests/alarm_handler/test_pointing_data_alarm.py
+++ tests/alarm_handler/test_pointing_data_alarm.py
@@ -9,8 +9,9 @@ from tests.resources.test_harness.helpers import retry_tango_command
 from tests.resources.test_support.constant import alarm_handler1
 
 
+@pytest.repeat(50)
 @pytest.mark.batch2
-@pytest.mark.SKA_mid
+@pytest.mark.SKA_mid44
 @scenario(
     "../features/test_harness/xtp_50437_nan_alarm.feature",
     "TMC mid raises an alarm when it encounters NaN"
