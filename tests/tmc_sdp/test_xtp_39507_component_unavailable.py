4d9eae227b8b3fb61f03da06650dc2e1efdeb382
devesh.ska <devesh.ska@gmail.com>
2025-04-16 19:44:12 +0530

    HM-655: update test

--- tests/tmc_sdp/test_xtp_39507_component_unavailable.py
+++ tests/tmc_sdp/test_xtp_39507_component_unavailable.py
@@ -148,7 +148,7 @@ def tmc_reports_unavailability_to_client(
     exception_message = (
         f" {tmc_subarraynode1}: Exception occurred on the"
         + f" following devices: {tmc_sdp_subarray_leaf_node}:"
-        + "The processing controller, helm deployer, or both are OFFLINE:"
+        + " The processing controller, helm deployer, or both are OFFLINE:"
         + " cannot start processing blocks."
     )
     assert "AssignResources" in pytest.assertion_data["attribute_value"][0]
