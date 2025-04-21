ef8a5b0790ed7fd34794773f3f971a899896c570
devesh.ska <devesh.ska@gmail.com>
2025-04-21 13:28:46 +0530

    HM-655: add test

--- tests/tmc_new_ITH/test_skb_837.py
+++ tests/tmc_new_ITH/test_skb_837.py
@@ -60,6 +60,7 @@ def _setup_event_subscriptions(
 
 @pytest.mark.new
 @pytest.mark.batch1
+@pytest.mark.SKA_mid
 @scenario(
     "../tmc_new_ITH/features/skb_837_receive_addresses_event_missing.feature",
     "Verify SKB-837",
