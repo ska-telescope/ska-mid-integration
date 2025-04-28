4394244173efd91572327b730d7d1110b921b731
Akshay Sonawane <akshayss.ska@gmail.com>
2025-04-28 10:34:20 +0530

    HM-662: test another scenario

--- tests/resources/test_harness/subarray_node.py
+++ tests/resources/test_harness/subarray_node.py
@@ -388,6 +388,7 @@ class SubarrayNodeWrapper(object):
             device = DeviceProxy(sim_device_fqdn)
             device.ResetDelayInfo()
             device.SetDirectHealthState(HealthState.UNKNOWN)
+            time.sleep(2)
             device.SetDefective(json.dumps({"enabled": False}))
 
     def _reset_dishes(self):
@@ -400,6 +401,7 @@ class SubarrayNodeWrapper(object):
             for dish_master in self.dish_master_list:
                 dish_master.SetDelay(2)
                 dish_master.SetDirectHealthState(HealthState.UNKNOWN)
+                time.sleep(2)
 
     def _clear_command_call_and_transition_data(self, clear_transition=False):
         """Clears the command call data"""
