a172e45bb84a4718bf6d7208e54cef528d08ed2b
devesh.ska <devesh.ska@gmail.com>
2025-05-06 11:45:35 +0530

    HM-669: update test

--- tests/tmc_robustness/Incremental_AssignResources_Failure_Handling_tests/test_incremental_assign_resources_sdp_subarray_failure.py
+++ tests/tmc_robustness/Incremental_AssignResources_Failure_Handling_tests/test_incremental_assign_resources_sdp_subarray_failure.py
@@ -79,7 +79,7 @@ def given_assign_resources_executed_on_tmc_subarray(
     command_input_factory,
 ):
     event_recorder.subscribe_event(
-        central_node_mid.central_node, "longRunningCommandResult"
+        central_node_mid.central_node, "longRunningCommandResult", timeout=130
     )
 
     assign_input_json = prepare_json_args_for_centralnode_commands(
@@ -131,6 +131,11 @@ def given_tmc_subarray_incremental_assign_resources_is_in_progress(
         central_node_mid.subarray_node,
         "obsState",
         ObsState.RESOURCING,
+    )
+    assert event_recorder.has_change_event_occurred(
+        central_node_mid.central_node,
+        "longRunningCommandResult",
+        (unique_id[0], Anything),
     )
 
 
