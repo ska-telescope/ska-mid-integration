c5a0a19f89e9e1d31262ae202acbcc3b75e29881
JayantKumbhar <jayant.ska@gmail.com>
2025-05-14 14:32:57 +0530

    SAH-1672: Debug test failure

--- tests/tmc_new_ITH/test_abort_timeout_propagation.py
+++ tests/tmc_new_ITH/test_abort_timeout_propagation.py
@@ -3,6 +3,7 @@ Test for Abort() error propagation verification
 """
 import json
 import logging
+import time
 
 import pytest
 from assertpy import assert_that
@@ -220,6 +221,7 @@ def verify_error_message(
         # tear_down as TMC is inconsistent state. Also
         # FAULT obsState is not considered in tear_down
         sdp.sdp_subarray.ResetDelayInfo()
+        time.sleep(120)
         assert_that(event_tracer).described_as(
             'FAILED ASSUMPTION IN "THEN" STEP: '
             "'the sdp subarray must be in the ABORTED obsState'"
