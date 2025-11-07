6cd4db83e9cdef781b057a76c3123b316c4ebbdb
Akshay Sonawane <akshayss.ska@gmail.com>
2025-11-07 12:19:41 +0530

    HM-785: update the test case

--- tests/tmc_new_ITH/test_array_layout.py
+++ tests/tmc_new_ITH/test_array_layout.py
@@ -13,9 +13,8 @@ from ska_integration_test_harness.facades.tmc_facade import TMCFacade
 from ska_tango_testing.integration import TangoEventTracer, log_events
 
 from tests.conftest import LOGGER
-from tests.tmc_csp_new_ITH.conftest import (
+from tests.tmc_csp_new_ITH.conftest import (  # SubarrayTestContextData,
     ASSERTIONS_TIMEOUT,
-    SubarrayTestContextData,
 )
 from tests.tmc_csp_new_ITH.utils.my_file_json_input import MyFileJSONInput
 from tests.tmc_new_ITH.utils.dpd_facade import DishPointingDevicesFacade
@@ -106,18 +105,32 @@ def given_assign_resources_executed_successfully(
         ),
     )
 
-    event_tracer.clear_events()
-
 
 @when("I invoke the Configure command on the SubarrayNode")
 def verify_version_sdp_mock_interface(
-    tmc: TMCFacade, context_fixt: SubarrayTestContextData
+    tmc: TMCFacade, event_tracer: TangoEventTracer
 ):
     """Invoke Configure command on SubarrayNode."""
     json_input = MyFileJSONInput("subarray", "command_Configure")
-    context_fixt.when_action_result = tmc.configure(
+    _, pytest.unique_id = tmc.configure(
         json_input,
         wait_termination=False,
+    )
+
+    assert_that(event_tracer).described_as(
+        "TMC subarray obsState should move to IDLE"
+    ).within_timeout(ASSERTIONS_TIMEOUT).has_change_event_occurred(
+        tmc.subarray_node, "obsState", ObsState.READY
+    )
+    assert_that(event_tracer).described_as(
+        "AssignResources command should complete successfully"
+    ).within_timeout(ASSERTIONS_TIMEOUT).has_change_event_occurred(
+        tmc.central_node,
+        "longRunningCommandResult",
+        (
+            pytest.unique_id[0],
+            json.dumps(((ResultCode.OK), "Command Completed")),
+        ),
     )
 
 
