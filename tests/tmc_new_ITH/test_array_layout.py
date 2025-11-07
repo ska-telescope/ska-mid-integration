dc3f40953a938bb38064f39c77c4b9de673e537e
Akshay Sonawane <akshayss.ska@gmail.com>
2025-11-07 14:20:57 +0530

    HM-785: update the test case

--- tests/tmc_new_ITH/test_array_layout.py
+++ tests/tmc_new_ITH/test_array_layout.py
@@ -15,6 +15,7 @@ from ska_tango_testing.integration import TangoEventTracer, log_events
 from tests.conftest import LOGGER
 from tests.tmc_csp_new_ITH.conftest import (  # SubarrayTestContextData,
     ASSERTIONS_TIMEOUT,
+    SubarrayTestContextData,
 )
 from tests.tmc_csp_new_ITH.utils.my_file_json_input import MyFileJSONInput
 from tests.tmc_new_ITH.utils.dpd_facade import DishPointingDevicesFacade
@@ -108,29 +109,42 @@ def given_assign_resources_executed_successfully(
 
 @when("I invoke the Configure command on the SubarrayNode")
 def verify_version_sdp_mock_interface(
-    tmc: TMCFacade, event_tracer: TangoEventTracer
+    tmc: TMCFacade,
+    context_fixt: SubarrayTestContextData,
+    event_tracer: TangoEventTracer,
+    csp,
+    sdp,
 ):
     """Invoke Configure command on SubarrayNode."""
     json_input = MyFileJSONInput("subarray", "command_Configure")
-    _, pytest.unique_id = tmc.configure(
+    context_fixt.when_action_result = tmc.configure(
         json_input,
+        wait_termination=False,
     )
-
+    context_fixt.starting_state = ObsState.CONFIGURING
     assert_that(event_tracer).described_as(
-        "TMC subarray obsState should move to IDLE"
-    ).within_timeout(ASSERTIONS_TIMEOUT).has_change_event_occurred(
-        tmc.subarray_node, "obsState", ObsState.READY
-    )
-    assert_that(event_tracer).described_as(
-        "AssignResources command should complete successfully"
+        f"Both TMC Subarray Node device ({tmc.subarray_node})"
+        f", CSP Subarray device ({csp.csp_subarray}) "
+        f"and SDP Subarray device ({sdp.sdp_subarray}) "
+        "ObsState attribute values should move "
+        f"from {str(context_fixt.starting_state)} to READY."
     ).within_timeout(ASSERTIONS_TIMEOUT).has_change_event_occurred(
         tmc.subarray_node,
-        "longRunningCommandResult",
-        (
-            pytest.unique_id[0],
-            json.dumps(((ResultCode.OK), "Command Completed")),
-        ),
+        "obsState",
+        ObsState.READY,
+        previous_value=context_fixt.starting_state,
+    ).has_change_event_occurred(
+        csp.csp_subarray,
+        "obsState",
+        ObsState.READY,
+        previous_value=context_fixt.starting_state,
+    ).has_change_event_occurred(
+        sdp.sdp_subarray,
+        "obsState",
+        ObsState.READY,
+        previous_value=context_fixt.starting_state,
     )
+    context_fixt.starting_state = ObsState.READY
 
 
 @then(
