c9dd59be19e054e1621e557679622cbbcb6bc8ef
Akshay Sonawane <akshayss.ska@gmail.com>
2025-11-10 14:22:10 +0530

    HM-785: update the test case

--- tests/tmc_new_ITH/test_array_layout.py
+++ tests/tmc_new_ITH/test_array_layout.py
@@ -286,17 +286,15 @@ def tmc_able_to_memorize_the_array_layout(
         f"dserver/{tmc.central_node.info().server_id}"
     )
     cn_device_server.restartserver()
+    assert wait_and_validate_device_attribute_value(
+        tmc.central_node,
+        "IsDishVccConfigSet",
+        "True",
+    )
     wait_and_validate_device_attribute_value(
         tmc.central_node,
         "state",
         "ON",
-        timeout=300,
-    )
-    tmc.move_to_off()
-    wait_and_validate_device_attribute_value(
-        tmc.central_node,
-        "telescopeState",
-        "OFF",
         timeout=300,
     )
     assert (
