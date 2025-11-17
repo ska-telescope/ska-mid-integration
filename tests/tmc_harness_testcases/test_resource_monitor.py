f0dcddbd25c28528d97257ad1469f0cd2e6a4641
manthan <manthan.ska@gmail.com>
2025-11-17 09:36:30 +0530

    SAH-1816: Execute test case

--- tests/tmc_harness_testcases/test_resource_monitor.py
+++ tests/tmc_harness_testcases/test_resource_monitor.py
@@ -4,7 +4,7 @@ and released resources change in the system.
 
 This test verifies that the ResourceMonitor device correctly updates its
 dishes attribute after resources are assigned and released through the
-SubarrayNode
+SubarrayNode.
 """
 
 import json
