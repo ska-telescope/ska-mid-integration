949865ceb7fd52724f90deaa4da7fad0532f7b2f
manthan <manthan.ska@gmail.com>
2025-11-20 11:34:30 +0530

    SAH-1816: Resolve review comment

--- tests/tmc_harness_testcases/test_resource_monitor.py
+++ tests/tmc_harness_testcases/test_resource_monitor.py
@@ -24,7 +24,7 @@ from tests.resources.test_harness.utils.common_utils import JsonFactory
 from tests.resources.test_support.constant import TIMEOUT, tmc_resource_monitor
 
 
-@pytest.mark.batch1
+@pytest.mark.batch2
 @pytest.mark.SKA_mid
 @scenario(
     "../features/resource_monitor.feature",
