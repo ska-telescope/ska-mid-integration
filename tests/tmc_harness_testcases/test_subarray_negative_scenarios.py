1367cb978a29a9cc3882a13f5b1c7cf9a5b30793
manthan <manthan.ska@gmail.com>
2026-01-27 15:05:34 +0530

    SAH-1849: run single test

--- tests/tmc_harness_testcases/test_subarray_negative_scenarios.py
+++ tests/tmc_harness_testcases/test_subarray_negative_scenarios.py
@@ -155,7 +155,7 @@ class TestSubarrayNodeNegative(object):
         )
         assert len(get_recorded_commands(sdp_sim)) == 1
 
-    @pytest.mark.batch2
+    @pytest.mark.batch2test
     @pytest.mark.SKA_mid
     def test_subarray_configure_when_dish_stuck_in_slew(
         self,
