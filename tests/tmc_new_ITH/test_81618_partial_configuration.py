2f28b413a35f695a278d21afb7973d44d6d51266
roshant.ska <roshant.ska@gmail.com>
2026-02-20 11:28:44 +0000

    HM-846: Fix calibration scan test case failure

--- tests/tmc_new_ITH/test_81618_partial_configuration.py
+++ tests/tmc_new_ITH/test_81618_partial_configuration.py
@@ -35,7 +35,8 @@ def update_configuration_json(config_json: dict, config_data: str):
     if config_data == "configuration_with_only_trajectory":
         config_json["pointing"].pop("wrap_sector", None)
     elif config_data == "configuration_with_only_band":
-        config_json.pop("pointing", None)
+        config_json["pointing"].clear()
+        config_json["pointing"]["wrap_sector"] = 0
         config_json["tmc"]["partial_configuration"] = False
         config_json["dish"] = {"receiver_band": "2"}
     elif config_data == "configuration_with_only_collimation_offsets":
@@ -59,7 +60,7 @@ def update_configuration_json(config_json: dict, config_data: str):
         config_json["pointing"]["wrap_sector"] = 0
 
 
-@pytest.mark.batch1
+@pytest.mark.test_f
 @pytest.mark.SKA_mid
 @scenario(
     "../tmc_new_ITH/features/xtp_81618_partial_configuration.feature",
