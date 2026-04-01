6ca76902604af5037d84a5e546d035b082ee8c3c
akshaysonawane <akshayss.ska@gmail.com>
2026-04-01 15:09:35 +0530

    HM-864: update tests

--- tests/tmc_harness_testcases/bdd_scenarios/test_science_scan_after_five_point_calibration.py
+++ tests/tmc_harness_testcases/bdd_scenarios/test_science_scan_after_five_point_calibration.py
@@ -28,7 +28,7 @@ from tests.resources.test_support.constant import (
 TIMEOUT = 110
 
 
-@pytest.mark.batch1
+@pytest.mark.aki1
 @pytest.mark.SKA_mid
 @scenario(
     "../features/test_harness/science_scan_after_calibration_scan.feature",
