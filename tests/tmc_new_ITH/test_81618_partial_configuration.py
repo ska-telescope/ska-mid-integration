1952136165382d117a270ba4971cb1794d60d819
akshaysonawane <akshayss.ska@gmail.com>
2026-04-02 11:42:45 +0530

    HM-864: update tests

--- tests/tmc_new_ITH/test_81618_partial_configuration.py
+++ tests/tmc_new_ITH/test_81618_partial_configuration.py
@@ -66,10 +66,6 @@ def update_configuration_json(config_json: dict, config_data: str):
 
 @pytest.mark.batch1
 @pytest.mark.SKA_mid
-@pytest.mark.xfail(
-    reason="The sourceOffset attribute does not match for scenario "
-    "configuration_with_traj_coll_offsets"
-)
 @scenario(
     "../tmc_new_ITH/features/xtp_81618_partial_configuration.feature",
     "TMC Behaviour when partial configuration is provided",
